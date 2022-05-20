41e6cf10dfb42854c2d16b34feaac9364b769e0e
Lymarenko Lev <lymarenko.lev@gmail.com>
2022-05-20 17:56:10 +0300

    fix typo

--- verification/src/http_server/routes.rs
+++ verification/src/http_server/routes.rs
@@ -9,11 +9,12 @@ pub fn routes(service_config: &mut web::ServiceConfig) {
     service_config
         .route("/health", web::get().to(get_health))
         .service(
-            web::scope("/api/v1")
-                .service(web::scope("/verification/"))
-                .route("/flatten/", web::post().to(source_code::verificate))
-                .route("/sourcify/", web::post().to(sourcify::verificate))
-                .route("/viper/", web::post().to(viper::verificate))
-                .route("/standard_json/", web::post().to(standard_json::verificate)),
+            web::scope("/api/v1").service(
+                web::scope("verification")
+                    .route("/flatten/", web::post().to(source_code::verificate))
+                    .route("/sourcify/", web::post().to(sourcify::verificate))
+                    .route("/viper/", web::post().to(viper::verificate))
+                    .route("/standard_json/", web::post().to(standard_json::verificate)),
+            ),
         );
 }
