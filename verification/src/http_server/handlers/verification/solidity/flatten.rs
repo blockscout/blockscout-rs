4019a42959db24c9a4097a2ca9018edc4da3fc5c
Lymarenko Lev <lymarenko.lev@gmail.com>
2022-05-23 16:20:47 +0300

    remove type parametr

--- verification/src/http_server/handlers/verification/solidity/flatten.rs
+++ verification/src/http_server/handlers/verification/solidity/flatten.rs
@@ -4,9 +4,9 @@ use log::debug;
 
 use super::types::{FlattenedSource, VerificationRequest, VerificationResponse};
 
-type Request = VerificationRequest<FlattenedSource>;
-
-pub async fn verify(params: Json<Request>) -> Result<Json<VerificationResponse>, Error> {
+pub async fn verify(
+    params: Json<VerificationRequest<FlattenedSource>>,
+) -> Result<Json<VerificationResponse>, Error> {
     debug!("verify contract with params {:?}", params);
     Ok(Json(VerificationResponse { verified: true }))
 }
