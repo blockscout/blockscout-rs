a6c98f3e9ce04a9c1bf6809723862b413dd6b7a2
Lymarenko Lev <lymarenko.lev@gmail.com>
2022-05-23 16:58:42 +0300

    remove middleware logger

--- verification/src/http_server/server.rs
+++ verification/src/http_server/server.rs
@@ -1,15 +1,13 @@
 use crate::config::Config;
 
 use super::routes;
-use actix_web::{middleware::Logger, App, HttpServer};
+use actix_web::{App, HttpServer};
 use log::info;
 
 pub async fn run_server(config: Config) -> std::io::Result<()> {
     info!("Verification server is starting at {}", config.socket_addr);
     HttpServer::new(move || {
-        let logger = Logger::default();
-
-        App::new().wrap(logger).configure(routes::config)
+        App::new().configure(routes::config)
     })
     .bind(config.socket_addr)?
     .run()
