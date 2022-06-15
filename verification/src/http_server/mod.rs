171a89e331e96db6dcbd63ce78567a1b5d50700e
Rim Rakhimov <rimrakhimov@gmail.com>
2022-06-15 15:55:51 +0300

    Update http_server: refactor server configuration

--- verification/src/http_server/mod.rs
+++ verification/src/http_server/mod.rs
@@ -10,13 +10,13 @@ use std::sync::Arc;
 pub async fn run(config: Config) -> std::io::Result<()> {
     let socket_addr = config.server.addr;
     log::info!("Verification server is starting at {}", socket_addr);
-    let app_config = Arc::new(
+    let app_router = Arc::new(
         AppRouter::new(config)
             .await
             .expect("couldn't initialize the app"),
     );
     HttpServer::new(move || {
-        App::new().configure(|service_config| app_config.register_routes(service_config))
+        App::new().configure(configure_router(&*app_router))
     })
     .bind(socket_addr)?
     .run()
