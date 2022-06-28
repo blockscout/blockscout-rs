40d04e74f744d157828bcc2e935f711f99bd646a
kuksag <georgiy.kuksa@gmail.com>
2022-06-28 10:38:24 +0300

    Import serde::Deserialize

--- multichain-api-gateway/src/config.rs
+++ multichain-api-gateway/src/config.rs
@@ -1,15 +1,15 @@
+use serde::Deserialize;
 use std::net::SocketAddr;
-
 use url::Url;
 
 /// An instance of the maintained networks in Blockscout.
 /// Semantic: (network, chain)
 /// e.g."blockscout.com/eth/mainnet" -> ("eth", "mainnet")
-#[derive(serde::Deserialize, Clone, Debug, PartialEq)]
+#[derive(Deserialize, Clone, Debug, PartialEq)]
 pub struct Instance(pub String, pub String);
 
 /// Settings for the Blockscout API
-#[derive(serde::Deserialize, Clone)]
+#[derive(Deserialize, Clone)]
 pub struct BlockScoutSettings {
     /// The base URL of the Blockscout API.
     /// At the moment, should be equal to "https://blockscout.com"
@@ -21,12 +21,12 @@ pub struct BlockScoutSettings {
     pub concurrent_requests: usize,
 }
 
-#[derive(serde::Deserialize, Clone)]
+#[derive(Deserialize, Clone)]
 pub struct ServerSettings {
     pub addr: SocketAddr,
 }
 
-#[derive(serde::Deserialize, Clone)]
+#[derive(Deserialize, Clone)]
 pub struct Settings {
     pub server: ServerSettings,
     pub blockscout: BlockScoutSettings,
