00e1cca372e6e9716b1afb7b9eed1259148e9ee2
Rim Rakhimov <rimrakhimov@gmail.com>
2022-06-28 12:39:14 +0300

    Updated standard json handler to use StandatdJson structu

--- verification/src/http_server/handlers/verification/solidity/standard_json.rs
+++ verification/src/http_server/handlers/verification/solidity/standard_json.rs
@@ -1,7 +1,7 @@
 use super::types::VerificationRequest;
 use crate::{
     compiler::{version::CompilerVersion, Compilers},
-    http_server::handlers::verification::VerificationResponse,
+    http_server::handlers::verification::{solidity::types::StandardJson, VerificationResponse},
     solidity::compiler_fetcher::CompilerFetcher,
 };
 use actix_web::{
@@ -9,17 +9,18 @@ use actix_web::{
     web::{self, Json},
     Error,
 };
-use ethers_solc::CompilerInput;
 use std::str::FromStr;
 
 pub async fn verify(
     compilers: web::Data<Compilers<CompilerFetcher>>,
-    params: Json<VerificationRequest<CompilerInput>>,
+    params: Json<VerificationRequest<StandardJson>>,
 ) -> Result<Json<VerificationResponse>, Error> {
     let params = params.into_inner();
     let compiler_version =
         CompilerVersion::from_str(&params.compiler_version).map_err(error::ErrorBadRequest)?;
-    let output = compilers.compile(&compiler_version, &params.content).await;
+    let output = compilers
+        .compile(&compiler_version, &params.content.into())
+        .await;
     let _ = output;
 
     todo!("verify output")
