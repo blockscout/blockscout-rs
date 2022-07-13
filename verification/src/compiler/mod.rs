968fe6d4febed9fe595ad021f78d577a3ddc916e
Lymarenko Lev <lymarenko.lev@gmail.com>
2022-07-13 16:52:30 +0300

    delete verification dir

--- verification/src/compiler/mod.rs
+++
@@ -1,9 +0,0 @@
-mod compilers;
-mod download_cache;
-mod fetcher;
-mod version;
-
-pub use compilers::{Compilers, CompilersError};
-pub use download_cache::DownloadCache;
-pub use fetcher::{Fetcher, VersionList};
-pub use version::CompilerVersion;
--- verification/src/http_server/handlers/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod status;
-pub mod verification;
-
-pub use self::verification::{
-    solidity::{multi_part, standard_json, version_list},
-    sourcify,
-};
