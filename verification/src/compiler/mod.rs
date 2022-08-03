c3c9b7eefee227e036d1e2ce405d2c4c938cf6e3
Lev Iskandarov <iskandarov11@inbox.ru>
2022-08-03 18:58:37 +0300

    added s3 fetcher

--- verification/src/compiler/mod.rs
+++ verification/src/compiler/mod.rs
@@ -2,10 +2,13 @@ mod compilers;
 mod download_cache;
 mod fetcher;
 mod list_fetcher;
+mod s3_fetcher;
 mod version;
+mod versions_fetcher;
 
 pub use compilers::{Compilers, Error};
 pub use download_cache::DownloadCache;
 pub use fetcher::Fetcher;
 pub use list_fetcher::ListFetcher;
+pub use s3_fetcher::S3Fetcher;
 pub use version::Version;
