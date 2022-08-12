b1e37279465ffa06800dd7a0ed54d714af8d19b1
kuksag <georgiy.kuksa@gmail.com>
2022-08-12 14:17:49 +0300

    Merged Get and Post request tests

--- multichain-api-gateway/tests/make_requests.rs
+++ multichain-api-gateway/tests/make_requests.rs
@@ -1,13 +1,18 @@
 use std::str;
 
-use actix_web::{test, web, web::Data, App};
+use actix_web::{
+    test, web,
+    web::{Bytes, Data},
+    App,
+};
 
 use multichain_api_gateway::{config, handle_request, ApiEndpoints};
 
 /// In the test we check that valid responses are returned from the API.
 /// Especially we call to the same network (xdai), but to different chains (mainnet, testnet).
+/// Also check correctness of POST/GET propagation
 #[actix_web::test]
-async fn check_get_requests() {
+async fn check_make_requests() {
     let settings = config::BlockscoutSettings {
         base_url: "https://blockscout.com".parse().unwrap(),
         instances: vec![
@@ -30,16 +35,26 @@ async fn check_get_requests() {
 
     let uri = "/api?module=block&action=getblockreward&blockno=0";
 
-    let req = test::TestRequest::get().uri(uri).to_request();
-
-    let bytes = test::call_and_read_body(&app, req).await;
-    let str = str::from_utf8(bytes.as_ref()).unwrap().to_string();
-    let actual_raw: serde_json::Value = serde_json::from_str(str.as_str()).unwrap();
-    let actual = serde_json::to_string_pretty(&actual_raw).unwrap();
-
-    let mut expected = std::fs::read_to_string("tests/res/get_requests.json").unwrap();
+    let mut expected = std::fs::read_to_string("tests/res/request_result.json").unwrap();
     // Remove trailing newline that comes from "read_to_string"
     expected.pop();
 
-    assert_eq!(expected, actual);
+    let get_request = test::TestRequest::get().uri(uri).to_request();
+    check_response(
+        test::call_and_read_body(&app, get_request).await,
+        expected.as_str(),
+    );
+
+    let post_request = test::TestRequest::get().uri(uri).to_request();
+    check_response(
+        test::call_and_read_body(&app, post_request).await,
+        expected.as_str(),
+    );
+}
+
+fn check_response(actual: Bytes, expected: &str) {
+    let str = str::from_utf8(actual.as_ref()).unwrap().to_string();
+    let actual_raw: serde_json::Value = serde_json::from_str(str.as_str()).unwrap();
+    let actual = serde_json::to_string_pretty(&actual_raw).unwrap();
+    assert_eq!(expected, actual.as_str());
 }
