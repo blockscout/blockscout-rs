e2cf3b1e2c47b43d31dc256edf2e0561d2d11433
Lev Iskandarov <iskandarov11@inbox.ru>
2022-08-23 13:51:13 +0300

    added openapi

--- smart-contract-service/build.rs
+++ smart-contract-service/build.rs
@@ -2,7 +2,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(false)
         .out_dir("src/proto")
-        .compile(&["proto/service.proto"], &["proto/", "proto/googleapis"])
+        .protoc_arg("--openapiv2_out=proto")
+        .compile(
+            &["proto/service.proto"],
+            &["proto/", "proto/googleapis", "proto/grpc-gateway"],
+        )
         .unwrap();
     Ok(())
 }
