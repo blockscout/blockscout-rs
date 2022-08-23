7c0c817cc1b33db669c67ea96a7d8b8f895502fd
Lev Iskandarov <iskandarov11@inbox.ru>
2022-08-23 16:11:00 +0300

    moved common deps into root folder

--- smart-contract-service/build.rs
+++ smart-contract-service/build.rs
@@ -5,7 +5,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .protoc_arg("--openapiv2_out=proto")
         .compile(
             &["proto/service.proto"],
-            &["proto/", "proto/googleapis", "proto/grpc-gateway"],
+            &["proto/", "../proto/googleapis", "../proto/grpc-gateway"],
         )
         .unwrap();
     Ok(())
