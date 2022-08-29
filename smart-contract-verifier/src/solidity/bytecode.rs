e641a67a82157a28c6fb291dbd39749a7e5f3b2d
Rim Rakhimov <rimrakhimov@gmail.com>
2022-08-30 00:44:07 +0300

    Verifier: added tests for LocalBytecode initialization

--- smart-contract-verifier/src/solidity/bytecode.rs
+++ smart-contract-verifier/src/solidity/bytecode.rs
@@ -257,3 +257,362 @@ impl LocalBytecode {
         Ok(parts)
     }
 }
+
+#[cfg(test)]
+mod local_bytecode_initialization_tests {
+    use super::*;
+    use const_format::concatcp;
+    use pretty_assertions::assert_eq;
+    use std::str::FromStr;
+
+    const CREATION_TX_INPUT_MAIN_PART_1: &'static str = "608060405234801561001057600080fd5b506040518060200161002190610050565b6020820181038252601f19601f820116604052506000908051906020019061004a92919061005c565b5061015f565b605c806101ac83390190565b8280546100689061012e565b90600052602060002090601f01602090048101928261008a57600085556100d1565b82601f106100a357805160ff19168380011785556100d1565b828001600101855582156100d1579182015b828111156100d05782518255916020019190600101906100b5565b5b5090506100de91906100e2565b5090565b5b808211156100fb5760008160009055506001016100e3565b5090565b7f4e487b7100000000000000000000000000000000000000000000000000000000600052602260045260246000fd5b6000600282049050600182168061014657607f821691505b602082108103610159576101586100ff565b5b50919050565b603f8061016d6000396000f3fe6080604052600080fdfe";
+    const CREATION_TX_INPUT_MAIN_PART_2: &'static str =
+        "6080604052348015600f57600080fd5b50603f80601d6000396000f3fe6080604052600080fdfe";
+
+    const DEPLOYED_BYTECODE_MAIN_PART_1: &'static str = "6080604052600080fdfe";
+    const DEPLOYED_BYTECODE_MAIN_PART_2: &'static str = "6080604052600080cafe";
+
+    const METADATA_PART_1: &'static str = "a26469706673582212202e82fb6222f966f0e56dc49cd1fb8a6b5eac9bdf74f62b8a5e9d8812901095d664736f6c634300080e0033";
+    const METADATA_PART_2: &'static str = "a2646970667358221220bd9f7fd5fb164e10dd86ccc9880d27a177e74ba873e6a9b97b6c4d7062b26ff064736f6c634300080e0033";
+
+    const METADATA_PART1_MODIFIED: &'static str = "a264697066735822122028c67e368422bc9c0b12226a099aa62a1facd39b08a84427d7f3efe1e37029b864736f6c634300080e0033";
+    const METADATA_PART2_MODIFIED: &'static str = "a26469706673582212206b331720b143820ca2e65d7db53a1b005672433fcb7f2da3ab539851bddc226a64736f6c634300080e0033";
+
+    const DEFAULT_DEPLOYED_BYTECODE: &'static str =
+        concatcp!(DEPLOYED_BYTECODE_MAIN_PART_1, METADATA_PART_1);
+    const DEFAULT_DEPLOYED_BYTECODE_MODIFIED: &'static str =
+        concatcp!(DEPLOYED_BYTECODE_MAIN_PART_1, METADATA_PART1_MODIFIED);
+
+    fn main_bytecode_part(raw: &str) -> BytecodePart {
+        let raw = DisplayBytes::from_str(raw)
+            .expect("Main bytecode part is invalid hex")
+            .0;
+        BytecodePart::Main { raw }
+    }
+
+    fn metadata_bytecode_part(raw: &str) -> BytecodePart {
+        let raw = DisplayBytes::from_str(raw)
+            .expect("Metadata bytecode part is invalid hex")
+            .0;
+        let (metadata, len) =
+            MetadataHash::from_cbor(&raw).expect("Metadata bytecode part is not metadata");
+        if raw.len() != len + 2 {
+            panic!("Metadata bytecode part has invalid length");
+        }
+        let metadata_length_raw = raw.slice(len..raw.len());
+        BytecodePart::Metadata {
+            metadata_raw: raw.slice(0..len),
+            metadata,
+            metadata_length_raw,
+        }
+    }
+
+    #[test]
+    fn without_metadata() {
+        let creation_tx_input = format!("{}", CREATION_TX_INPUT_MAIN_PART_1);
+        let deployed_bytecode = format!("{}", DEPLOYED_BYTECODE_MAIN_PART_1);
+
+        let creation_tx_input_modified = format!("{}", CREATION_TX_INPUT_MAIN_PART_1);
+        let deployed_bytecode_modified = format!("{}", DEPLOYED_BYTECODE_MAIN_PART_1);
+
+        let bytecode = Bytecode::new(&creation_tx_input, &deployed_bytecode)
+            .expect("Bytecode initialization failed");
+        let bytecode_modified =
+            Bytecode::new(&creation_tx_input_modified, &deployed_bytecode_modified)
+                .expect("Modified bytecode initialization failed");
+
+        let local_bytecode = LocalBytecode::new(bytecode.clone(), bytecode_modified);
+
+        let local_bytecode = local_bytecode.expect("Initialization of local bytecode failed");
+        assert_eq!(bytecode, local_bytecode.bytecode, "Invalid bytecode");
+        assert_eq!(
+            vec![main_bytecode_part(CREATION_TX_INPUT_MAIN_PART_1)],
+            local_bytecode.creation_tx_input_parts,
+            "Invalid bytecode parts"
+        );
+    }
+
+    #[test]
+    fn with_one_metadata() {
+        let creation_tx_input = format!("{}{}", CREATION_TX_INPUT_MAIN_PART_1, METADATA_PART_1);
+        let deployed_bytecode = format!("{}{}", DEPLOYED_BYTECODE_MAIN_PART_1, METADATA_PART_1);
+
+        let creation_tx_input_modified = format!(
+            "{}{}",
+            CREATION_TX_INPUT_MAIN_PART_1, METADATA_PART1_MODIFIED
+        );
+        let deployed_bytecode_modified = format!(
+            "{}{}",
+            DEPLOYED_BYTECODE_MAIN_PART_1, METADATA_PART1_MODIFIED
+        );
+
+        let bytecode = Bytecode::new(&creation_tx_input, &deployed_bytecode)
+            .expect("Bytecode initialization failed");
+        let bytecode_modified =
+            Bytecode::new(&creation_tx_input_modified, &deployed_bytecode_modified)
+                .expect("Modified bytecode initialization failed");
+
+        let local_bytecode = LocalBytecode::new(bytecode.clone(), bytecode_modified);
+
+        let local_bytecode = local_bytecode.expect("Initialization of local bytecode failed");
+        assert_eq!(bytecode, local_bytecode.bytecode, "Invalid bytecode");
+        assert_eq!(
+            vec![
+                main_bytecode_part(CREATION_TX_INPUT_MAIN_PART_1),
+                metadata_bytecode_part(METADATA_PART_1)
+            ],
+            local_bytecode.creation_tx_input_parts,
+            "Invalid bytecode parts"
+        );
+    }
+
+    #[test]
+    fn with_two_metadata() {
+        let creation_tx_input = format!(
+            "{}{}{}{}",
+            CREATION_TX_INPUT_MAIN_PART_1,
+            METADATA_PART_1,
+            CREATION_TX_INPUT_MAIN_PART_2,
+            METADATA_PART_2
+        );
+        let deployed_bytecode = format!(
+            "{}{}{}{}",
+            DEPLOYED_BYTECODE_MAIN_PART_1,
+            METADATA_PART_1,
+            DEPLOYED_BYTECODE_MAIN_PART_2,
+            METADATA_PART_2
+        );
+
+        let creation_tx_input_modified = format!(
+            "{}{}{}{}",
+            CREATION_TX_INPUT_MAIN_PART_1,
+            METADATA_PART1_MODIFIED,
+            CREATION_TX_INPUT_MAIN_PART_2,
+            METADATA_PART2_MODIFIED
+        );
+        let deployed_bytecode_modified = format!(
+            "{}{}{}{}",
+            DEPLOYED_BYTECODE_MAIN_PART_1,
+            METADATA_PART1_MODIFIED,
+            DEPLOYED_BYTECODE_MAIN_PART_2,
+            METADATA_PART2_MODIFIED
+        );
+
+        let bytecode = Bytecode::new(&creation_tx_input, &deployed_bytecode)
+            .expect("Bytecode initialization failed");
+        let bytecode_modified =
+            Bytecode::new(&creation_tx_input_modified, &deployed_bytecode_modified)
+                .expect("Modified bytecode initialization failed");
+
+        let local_bytecode = LocalBytecode::new(bytecode.clone(), bytecode_modified);
+
+        let local_bytecode = local_bytecode.expect("Initialization of local bytecode failed");
+        assert_eq!(bytecode, local_bytecode.bytecode, "Invalid bytecode");
+        assert_eq!(
+            vec![
+                main_bytecode_part(CREATION_TX_INPUT_MAIN_PART_1),
+                metadata_bytecode_part(METADATA_PART_1),
+                main_bytecode_part(CREATION_TX_INPUT_MAIN_PART_2),
+                metadata_bytecode_part(METADATA_PART_2),
+            ],
+            local_bytecode.creation_tx_input_parts,
+            "Invalid bytecode parts"
+        );
+    }
+
+    #[test]
+    fn with_two_metadata_but_one_main_part() {
+        let creation_tx_input = format!(
+            "{}{}{}",
+            CREATION_TX_INPUT_MAIN_PART_1, METADATA_PART_1, METADATA_PART_2
+        );
+        let deployed_bytecode = format!(
+            "{}{}{}",
+            DEPLOYED_BYTECODE_MAIN_PART_1, METADATA_PART_1, METADATA_PART_2
+        );
+
+        let creation_tx_input_modified = format!(
+            "{}{}{}",
+            CREATION_TX_INPUT_MAIN_PART_1, METADATA_PART1_MODIFIED, METADATA_PART2_MODIFIED
+        );
+        let deployed_bytecode_modified = format!(
+            "{}{}{}",
+            DEPLOYED_BYTECODE_MAIN_PART_1, METADATA_PART1_MODIFIED, METADATA_PART2_MODIFIED
+        );
+
+        let bytecode = Bytecode::new(&creation_tx_input, &deployed_bytecode)
+            .expect("Bytecode initialization failed");
+        let bytecode_modified =
+            Bytecode::new(&creation_tx_input_modified, &deployed_bytecode_modified)
+                .expect("Modified bytecode initialization failed");
+
+        let local_bytecode = LocalBytecode::new(bytecode.clone(), bytecode_modified);
+
+        let local_bytecode = local_bytecode.expect("Initialization of local bytecode failed");
+        assert_eq!(bytecode, local_bytecode.bytecode, "Invalid bytecode");
+        assert_eq!(
+            vec![
+                main_bytecode_part(CREATION_TX_INPUT_MAIN_PART_1),
+                metadata_bytecode_part(METADATA_PART_1),
+                metadata_bytecode_part(METADATA_PART_2),
+            ],
+            local_bytecode.creation_tx_input_parts,
+            "Invalid bytecode parts"
+        );
+    }
+
+    #[test]
+    fn with_different_lengths_should_fail() {
+        let creation_tx_input = format!("{}{}", CREATION_TX_INPUT_MAIN_PART_1, METADATA_PART_1);
+
+        // additional byte
+        let creation_tx_input_modified = format!(
+            "{}{}12",
+            CREATION_TX_INPUT_MAIN_PART_1, METADATA_PART1_MODIFIED
+        );
+
+        let bytecode = Bytecode::new(&creation_tx_input, DEFAULT_DEPLOYED_BYTECODE)
+            .expect("Bytecode initialization failed");
+        let bytecode_modified = Bytecode::new(
+            &creation_tx_input_modified,
+            DEFAULT_DEPLOYED_BYTECODE_MODIFIED,
+        )
+        .expect("Modified bytecode initialization failed");
+
+        let local_bytecode = LocalBytecode::new(bytecode.clone(), bytecode_modified);
+
+        assert!(
+            local_bytecode.is_err(),
+            "Should fail, but: {:?}",
+            local_bytecode.unwrap()
+        );
+        match local_bytecode.unwrap_err() {
+            VerificationErrorKind::InternalError(error) => {
+                assert!(
+                    error.contains("length mismatch"),
+                    "Invalid error message: {}",
+                    error
+                )
+            }
+            _ => panic!("Invalid error"),
+        }
+    }
+
+    #[test]
+    fn with_invalid_metadata_should_fail() {
+        let creation_tx_input = format!("{}cafe{}", CREATION_TX_INPUT_MAIN_PART_1, METADATA_PART_1);
+
+        let creation_tx_input_modified = format!(
+            "{}abcd{}",
+            CREATION_TX_INPUT_MAIN_PART_1, METADATA_PART1_MODIFIED
+        );
+
+        let bytecode = Bytecode::new(&creation_tx_input, DEFAULT_DEPLOYED_BYTECODE)
+            .expect("Bytecode initialization failed");
+        let bytecode_modified = Bytecode::new(
+            &creation_tx_input_modified,
+            DEFAULT_DEPLOYED_BYTECODE_MODIFIED,
+        )
+        .expect("Modified bytecode initialization failed");
+
+        let local_bytecode = LocalBytecode::new(bytecode.clone(), bytecode_modified);
+
+        assert!(
+            local_bytecode.is_err(),
+            "Should fail, but: {:?}",
+            local_bytecode.unwrap()
+        );
+        match local_bytecode.unwrap_err() {
+            VerificationErrorKind::InternalError(error) => {
+                assert!(
+                    error.contains("failed to parse bytecode part"),
+                    "Invalid error message: {}",
+                    error
+                )
+            }
+            _ => panic!("Invalid error"),
+        }
+    }
+
+    #[test]
+    fn with_absent_metadata_length_should_fail() {
+        let creation_tx_input = format!(
+            "{}{}",
+            CREATION_TX_INPUT_MAIN_PART_1,
+            &METADATA_PART_1[..METADATA_PART_1.len() - 2]
+        );
+
+        let creation_tx_input_modified = format!(
+            "{}{}",
+            CREATION_TX_INPUT_MAIN_PART_1,
+            &METADATA_PART1_MODIFIED[..METADATA_PART1_MODIFIED.len() - 2]
+        );
+
+        let bytecode = Bytecode::new(&creation_tx_input, DEFAULT_DEPLOYED_BYTECODE)
+            .expect("Bytecode initialization failed");
+        let bytecode_modified = Bytecode::new(
+            &creation_tx_input_modified,
+            DEFAULT_DEPLOYED_BYTECODE_MODIFIED,
+        )
+        .expect("Modified bytecode initialization failed");
+
+        let local_bytecode = LocalBytecode::new(bytecode.clone(), bytecode_modified);
+
+        assert!(
+            local_bytecode.is_err(),
+            "Should fail, but: {:?}",
+            local_bytecode.unwrap()
+        );
+        match local_bytecode.unwrap_err() {
+            VerificationErrorKind::InternalError(error) => {
+                assert!(error.contains(""), "Invalid error message: {}", error)
+            }
+            _ => panic!("failed to parse metadata length"),
+        }
+    }
+
+    #[test]
+    fn with_invalid_metadata_length_should_fail() {
+        let creation_tx_input = format!(
+            "{}{}{}",
+            CREATION_TX_INPUT_MAIN_PART_1,
+            &METADATA_PART_1[..METADATA_PART_1.len() - 4],
+            "0031"
+        );
+
+        let creation_tx_input_modified = format!(
+            "{}{}{}",
+            CREATION_TX_INPUT_MAIN_PART_1,
+            &METADATA_PART1_MODIFIED[..METADATA_PART1_MODIFIED.len() - 4],
+            "0031"
+        );
+
+        let bytecode = Bytecode::new(&creation_tx_input, DEFAULT_DEPLOYED_BYTECODE)
+            .expect("Bytecode initialization failed");
+        let bytecode_modified = Bytecode::new(
+            &creation_tx_input_modified,
+            DEFAULT_DEPLOYED_BYTECODE_MODIFIED,
+        )
+        .expect("Modified bytecode initialization failed");
+
+        let local_bytecode = LocalBytecode::new(bytecode.clone(), bytecode_modified);
+
+        assert!(
+            local_bytecode.is_err(),
+            "Should fail, but: {:?}",
+            local_bytecode.unwrap()
+        );
+        match local_bytecode.unwrap_err() {
+            VerificationErrorKind::InternalError(error) => {
+                assert!(
+                    error.contains(
+                        "encoded metadata length does not correspond to actual metadata length"
+                    ),
+                    "Invalid error message: {}",
+                    error
+                )
+            }
+            _ => panic!("failed to parse metadata length"),
+        }
+    }
+}
