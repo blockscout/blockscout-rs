13014153a79d9d4a1a4d87dd4ff1a7ce631adb00
Rim Rakhimov <rimrakhimov@gmail.com>
2022-09-04 13:59:47 +0300

    Fix tests; add basic sourcify

--- smart-contract-verifier-http/src/lib.rs
+++ smart-contract-verifier-http/src/lib.rs
@@ -5,6 +5,9 @@ mod run;
 mod settings;
 mod tracer;
 
+#[cfg(test)]
+mod tests;
+
 pub use ethers_core::types::Bytes as DisplayBytes;
 
 pub use run::run;
---
+++ smart-contract-verifier-http/src/tests/parse.rs
@@ -0,0 +1,26 @@
+use pretty_assertions::assert_eq;
+use serde::{de::DeserializeOwned, Serialize};
+use serde_json::Value;
+use std::fmt::Debug;
+
+pub fn test_deserialize_ok<T>(tests: Vec<(&str, T)>)
+where
+    T: Debug + PartialEq + DeserializeOwned,
+{
+    for (s, value) in tests {
+        let v: T = serde_json::from_str(s).unwrap();
+        assert_eq!(v, value);
+    }
+}
+
+pub fn test_serialize_json_ok<T>(tests: Vec<(T, Value)>)
+where
+    T: Serialize,
+{
+    for (object, expected_json) in tests {
+        let object_string = serde_json::to_string(&object).unwrap();
+        let object_json: Value = serde_json::from_str(&object_string).unwrap();
+
+        assert_eq!(object_json, expected_json);
+    }
+}
