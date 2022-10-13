4f2972e01736c3ca1c57c5175ac6d3ec583efa9e
Rim Rakhimov <rimrakhimov@gmail.com>
2022-10-13 16:42:13 +0300

    smart-contract-verifier: add comments

--- smart-contract-verifier/src/middleware.rs
+++ smart-contract-verifier/src/middleware.rs
@@ -1,3 +1,8 @@
+/// When implemented the struct could be added as a verification success
+/// post-processing step.
+//
+// Output type implemented as a generic and not as an associated type,
+// so that the same middleware provider could process different output types.
 #[async_trait::async_trait]
 pub trait Middleware<Output>: 'static + Send + Sync {
     /// Invoked with a verification output after successful verification.
