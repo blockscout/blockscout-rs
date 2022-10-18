2b3029a567ca624961de989032809cc8236074c5
Rim Rakhimov <rimrakhimov@gmail.com>
2022-10-18 18:07:38 +0300

    smart-contract-verifier: Add 'Composition' middleware that combine several middlewares and execute every one of them. Added and exposed for convenience purpose, as it seems to be the most common way to combine several middlewares

--- smart-contract-verifier/src/middleware.rs
+++ smart-contract-verifier/src/middleware.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 /// When implemented the struct could be added as a verification success
 /// post-processing step.
 //
@@ -10,4 +12,76 @@ pub trait Middleware<Output>: 'static + Send + Sync {
     /// All errors should be handled inside the middleware and should not
     /// be propagated to the client that called it.
     async fn call(&self, output: &Output) -> ();
+}
+
+#[derive(Default)]
+pub struct Composition<Output> {
+    middleware_stack: Vec<Arc<dyn Middleware<Output>>>,
+}
+
+impl<Output> Composition<Output> {
+    /// Initialize empty composition.
+    pub fn new() -> Self {
+        Self {
+            middleware_stack: vec![],
+        }
+    }
+
+    /// Convenience method to attach middleware.
+    ///
+    /// If you need to keep a reference to the middleware after attaching, use [`with_arc`].
+    ///
+    /// [`with_arc`]: Self::with_arc
+    pub fn with<M>(self, middleware: M) -> Self
+    where
+        M: Middleware<Output>,
+    {
+        self.with_arc(Arc::new(middleware))
+    }
+
+    /// Add middleware to the composition. [`with`] is more ergonomic if you don't need the `Arc`.
+    ///
+    /// [`with`]: Self::with
+    pub fn with_arc(mut self, middleware: Arc<dyn Middleware<Output>>) -> Self {
+        self.middleware_stack.push(middleware);
+        self
+    }
+}
+
+#[async_trait::async_trait]
+impl<Output: 'static + Sync> Middleware<Output> for Composition<Output> {
+    async fn call(&self, output: &Output) -> () {
+        for middleware in &self.middleware_stack {
+            middleware.call(output).await
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use mockall::mock;
+
+    mock! {
+        Middleware<Output: 'static + Send + Sync> {}
+
+        #[async_trait::async_trait]
+        impl<Output: 'static + Send + Sync> super::Middleware<Output> for Middleware<Output> {
+            async fn call(&self, output: &Output) -> ();
+        }
+    }
+
+    #[tokio::test]
+    async fn composition() {
+        let mut middleware1 = MockMiddleware::<()>::new();
+        let mut middleware2 = MockMiddleware::<()>::new();
+
+        middleware1.expect_call().times(1).return_const(());
+        middleware2.expect_call().times(1).return_const(());
+
+        let composition = Composition::new()
+            .with(middleware1)
+            .with_arc(Arc::new(middleware2));
+        composition.call(&()).await;
+    }
 }
