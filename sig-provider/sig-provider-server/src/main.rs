a1925f070c125987fc4df189564561f81ee34a57
Iskandarov Lev <iskandarov11@inbox.ru>
2022-11-11 20:37:20 +0400

    updated actix prost and fixed logging bug (#182)

--- sig-provider/sig-provider-server/src/main.rs
+++ sig-provider/sig-provider-server/src/main.rs
@@ -2,7 +2,6 @@ use sig_provider_server::{sig_provider, Settings};
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    env_logger::init();
     let settings = Settings::new().expect("failed to read config");
     sig_provider(settings).await
 }
