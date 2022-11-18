331127d34f45b9c141ff2673a23f094aeb0a481e
Rim Rakhimov <rimrakhimov@gmail.com>
2022-11-18 17:26:19 +0400

    Cargo.lock; cargo fmt

--- eth-bytecode-db/eth-bytecode-db-proto/src/lib.rs
+++ eth-bytecode-db/eth-bytecode-db-proto/src/lib.rs
@@ -2,7 +2,10 @@
 pub mod blockscout {
     pub mod eth_bytecode_db {
         pub mod v1 {
-            include!(concat!(env!("OUT_DIR"), "/blockscout.eth_bytecode_db.v1.rs"));
+            include!(concat!(
+                env!("OUT_DIR"),
+                "/blockscout.eth_bytecode_db.v1.rs"
+            ));
         }
     }
 }
