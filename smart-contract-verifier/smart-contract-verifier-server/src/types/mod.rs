315ad5e173e87579585225173bcf89276caa1d8d
Rim Rakhimov <rimrakhimov@gmail.com>
2022-11-24 16:26:12 +0300

    Add VerifyResponseWrapper type

---
+++ smart-contract-verifier/smart-contract-verifier-server/src/types/mod.rs
@@ -0,0 +1,5 @@
+// mod solidity_multi_part;
+mod verification_response;
+
+// pub use solidity_multi_part::VerifySolidityMultiPartRequestWrapper;
+pub use verification_response::VerifyResponseWrapper;
