7887a1dd2b5ba80eb997eeac5ec7239eb7c8915d
Rim Rakhimov <rimrakhimov@gmail.com>
2022-11-24 13:42:07 +0300

    Initialize SolidityVerifierService

--- smart-contract-verifier/smart-contract-verifier-server/src/services/solidity_verifier.rs
+++ smart-contract-verifier/smart-contract-verifier-server/src/services/solidity_verifier.rs
@@ -1,11 +1,60 @@
+use crate::settings::{FetcherSettings, S3FetcherSettings, SoliditySettings};
+use s3::{creds::Credentials, Bucket, Region};
+use smart_contract_verifier::{
+    Compilers, Fetcher, ListFetcher, S3Fetcher, SolcValidator, SolidityClient, SolidityCompiler,
+};
 use smart_contract_verifier_proto::blockscout::smart_contract_verifier::v1::{
     solidity_verifier_server::SolidityVerifier, ListVersionsRequest, ListVersionsResponse,
     VerifyResponse, VerifySolidityMultiPartRequest, VerifySolidityStandardJsonRequest,
 };
+use std::{str::FromStr, sync::Arc};
+use tokio::sync::Semaphore;
 use tonic::{Request, Response, Status};
 
-#[derive(Default)]
-pub struct SolidityVerifierService {}
+pub struct SolidityVerifierService {
+    _client: Arc<SolidityClient>,
+}
+
+impl SolidityVerifierService {
+    pub async fn new(
+        settings: SoliditySettings,
+        compilers_threads_semaphore: Arc<Semaphore>,
+    ) -> anyhow::Result<Self> {
+        let dir = settings.compilers_dir.clone();
+        let schedule = settings.refresh_versions_schedule;
+        let validator = Arc::new(SolcValidator::default());
+        let fetcher: Arc<dyn Fetcher> = match settings.fetcher {
+            FetcherSettings::List(list_settings) => Arc::new(
+                ListFetcher::new(
+                    list_settings.list_url,
+                    settings.compilers_dir,
+                    Some(schedule),
+                    Some(validator),
+                )
+                .await?,
+            ),
+            FetcherSettings::S3(s3_settings) => Arc::new(
+                S3Fetcher::new(
+                    new_bucket(&s3_settings)?,
+                    settings.compilers_dir,
+                    Some(schedule),
+                    Some(validator),
+                )
+                .await?,
+            ),
+        };
+        let compilers = Compilers::new(
+            fetcher,
+            SolidityCompiler::new(),
+            compilers_threads_semaphore,
+        );
+        compilers.load_from_dir(&dir).await;
+        let client = SolidityClient::new(compilers);
+        Ok(Self {
+            _client: Arc::new(client),
+        })
+    }
+}
 
 #[async_trait::async_trait]
 impl SolidityVerifier for SolidityVerifierService {
@@ -29,4 +78,38 @@ impl SolidityVerifier for SolidityVerifierService {
     ) -> Result<Response<ListVersionsResponse>, Status> {
         todo!()
     }
+}
+
+fn new_region(region: Option<String>, endpoint: Option<String>) -> Option<Region> {
+    let region = region.unwrap_or_default();
+    if let Some(endpoint) = endpoint {
+        return Some(Region::Custom { region, endpoint });
+    }
+
+    // try to match with AWS regions, fail otherwise
+    let region = Region::from_str(&region).ok()?;
+    match region {
+        Region::Custom {
+            region: _,
+            endpoint: _,
+        } => None,
+        region => Some(region),
+    }
+}
+
+fn new_bucket(settings: &S3FetcherSettings) -> anyhow::Result<Arc<Bucket>> {
+    let region = new_region(settings.region.clone(), settings.endpoint.clone())
+        .ok_or_else(|| anyhow::anyhow!("got invalid region/endpoint settings"))?;
+    let bucket = Arc::new(Bucket::new(
+        &settings.bucket,
+        region,
+        Credentials::new(
+            settings.access_key.as_deref(),
+            settings.secret_key.as_deref(),
+            None,
+            None,
+            None,
+        )?,
+    )?);
+    Ok(bucket)
 }
