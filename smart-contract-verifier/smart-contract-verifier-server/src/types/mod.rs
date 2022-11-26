100b38528a7d7a426ddbd51901b6cd10cf5c4bd6
Rim Rakhimov <rimrakhimov@gmail.com>
2022-11-27 01:09:33 +0300

    Update VerifyVyperMultiPartRequest 'evm_version' field to be optional. Add VerifyVyperMultiPartRequestWrapper

--- smart-contract-verifier/smart-contract-verifier-server/src/types/mod.rs
+++ smart-contract-verifier/smart-contract-verifier-server/src/types/mod.rs
@@ -1,9 +1,11 @@
 mod solidity_multi_part;
 mod solidity_standard_json;
 mod verification_response;
+mod vyper_multi_part;
 
 pub use solidity_multi_part::VerifySolidityMultiPartRequestWrapper;
 pub use solidity_standard_json::{
     ParseError as StandardJsonParseError, VerifySolidityStandardJsonRequestWrapper,
 };
 pub use verification_response::VerifyResponseWrapper;
+pub use vyper_multi_part::VerifyVyperMultiPartRequestWrapper;
