5b2761f88794a8e0cb082cdb3d1efedebec5fa32
Lev Iskandarov <iskandarov11@inbox.ru>
2022-11-28 19:40:50 +0300

    mock api

--- stats/stats-server/src/service.rs
+++ stats/stats-server/src/service.rs
@@ -1,20 +1,33 @@
 use async_trait::async_trait;
-use stats_proto::blockscout::stats::v1::{stats_server::Stats, GetStatsRequest, GetStatsResponse};
+use stats_proto::blockscout::stats::v1::{
+    stats_service_server::StatsService, Chart, ChartRequest, Point, Stats, StatsRequest,
+};
+use tonic::{Request, Response, Status};
 
-pub struct StatsService {}
+pub struct Service {}
 
-impl StatsService {
+impl Service {
     pub fn new() -> Self {
         Self {}
     }
 }
 
 #[async_trait]
-impl Stats for StatsService {
-    async fn get_stats(
+impl StatsService for Service {
+    async fn get_stats(&self, _request: Request<StatsRequest>) -> Result<Response<Stats>, Status> {
+        Ok(Response::new(Stats {
+            total_blocks_all_time: 1,
+        }))
+    }
+    async fn get_new_blocks(
         &self,
-        _request: tonic::Request<GetStatsRequest>,
-    ) -> Result<tonic::Response<GetStatsResponse>, tonic::Status> {
-        unimplemented!();
+        _request: Request<ChartRequest>,
+    ) -> Result<Response<Chart>, Status> {
+        Ok(Response::new(Chart {
+            chart: vec![Point {
+                date: "01-01-2022".into(),
+                value: 123,
+            }],
+        }))
     }
 }
