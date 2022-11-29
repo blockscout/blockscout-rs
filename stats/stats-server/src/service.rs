a92ade1608bfab9f1e30f1df8c6ed16b2c2f0c22
Lev Iskandarov <iskandarov11@inbox.ru>
2022-11-29 16:37:22 +0300

    some more interesting mock

--- stats/stats-server/src/service.rs
+++ stats/stats-server/src/service.rs
@@ -1,6 +1,9 @@
+use std::str::FromStr;
+
 use async_trait::async_trait;
+use chrono::{Duration, NaiveDateTime};
 use stats_proto::blockscout::stats::v1::{
-    stats_service_server::StatsService, Chart, ChartRequest, Point, Stats, StatsRequest,
+    stats_service_server::StatsService, Chart, ChartRequest, Counters, CountersRequest, Point,
 };
 use tonic::{Request, Response, Status};
 
@@ -14,20 +17,26 @@ impl Service {
 
 #[async_trait]
 impl StatsService for Service {
-    async fn get_stats(&self, _request: Request<StatsRequest>) -> Result<Response<Stats>, Status> {
-        Ok(Response::new(Stats {
-            total_blocks_all_time: 1,
+    async fn get_counters(
+        &self,
+        _request: Request<CountersRequest>,
+    ) -> Result<Response<Counters>, Status> {
+        Ok(Response::new(Counters {
+            total_blocks_all_time: 16075890,
         }))
     }
     async fn get_new_blocks(
         &self,
         _request: Request<ChartRequest>,
     ) -> Result<Response<Chart>, Status> {
-        Ok(Response::new(Chart {
-            chart: vec![Point {
-                date: "01-01-2022".into(),
-                value: 123,
-            }],
-        }))
+        let start = NaiveDateTime::from_str("2022-01-01T00:00:00").unwrap();
+        let chart = (0..90)
+            .into_iter()
+            .map(|i| Point {
+                date: (start + Duration::days(i)).format("%d-%m-%Y").to_string(),
+                value: 100 + (i as u64 % 10),
+            })
+            .collect();
+        Ok(Response::new(Chart { chart }))
     }
 }
