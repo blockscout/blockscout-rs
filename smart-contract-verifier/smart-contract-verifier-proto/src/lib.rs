f6b5c0ac1c5773f3d5643d4386487799a95c3065
Rim Rakhimov <rimrakhimov@gmail.com>
2022-12-21 02:14:53 +0300

    Update lib.rs to v2. Add extra data for VerifyResponse

--- smart-contract-verifier/smart-contract-verifier-proto/src/lib.rs
+++ smart-contract-verifier/smart-contract-verifier-proto/src/lib.rs
@@ -1,7 +1,7 @@
 #![allow(clippy::derive_partial_eq_without_eq)]
 pub mod blockscout {
     pub mod smart_contract_verifier {
-        pub mod v1 {
+        pub mod v2 {
             include!(concat!(
                 env!("OUT_DIR"),
                 "/blockscout.smart_contract_verifier.v2.rs"
