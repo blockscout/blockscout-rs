28d992f90f65b9cb772253470acf560ae9f083f5
Lev Iskandarov <iskandarov11@inbox.ru>
2022-12-23 18:49:03 +0300

    fmt

--- stats/stats/src/lib.rs
+++ stats/stats/src/lib.rs
@@ -1,8 +1,8 @@
-mod read;
 mod chart;
+mod read;
 
 pub use entity;
 pub use migration;
 
-pub use read::{get_chart_int, get_counters, ReadError};
 pub use chart::{mock, new_blocks::NewBlocks, total_blocks::TotalBlocks, Chart, UpdateError};
+pub use read::{get_chart_int, get_counters, ReadError};
