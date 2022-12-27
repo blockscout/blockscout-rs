1966ae180cec3816c199c0624c013e5fcfa60418
Rim Rakhimov <rimrakhimov@gmail.com>
2022-12-28 00:09:52 +0300

    Implement solidity verify_standard_json

--- eth-bytecode-db/eth-bytecode-db-server/src/services/solidity_verifier.rs
+++ eth-bytecode-db/eth-bytecode-db-server/src/services/solidity_verifier.rs
@@ -7,7 +7,9 @@ use crate::{
 };
 use amplify::Wrapper;
 use async_trait::async_trait;
-use eth_bytecode_db::verification::{solidity_multi_part, Client, Error, VerificationRequest};
+use eth_bytecode_db::verification::{
+    solidity_multi_part, solidity_standard_json, Client, Error, VerificationRequest,
+};
 
 pub struct SolidityVerifierService {
     client: Client,
@@ -57,9 +59,34 @@ impl solidity_verifier_server::SolidityVerifier for SolidityVerifierService {
 
     async fn verify_standard_json(
         &self,
-        _request: tonic::Request<VerifySolidityStandardJsonRequest>,
+        request: tonic::Request<VerifySolidityStandardJsonRequest>,
     ) -> Result<tonic::Response<VerifyResponse>, tonic::Status> {
-        todo!()
+        let request = request.into_inner();
+
+        let bytecode_type = request.bytecode_type();
+        let verification_request = VerificationRequest {
+            bytecode: request.bytecode,
+            bytecode_type: BytecodeTypeWrapper::from_inner(bytecode_type).try_into()?,
+            compiler_version: request.compiler_version,
+            content: solidity_standard_json::StandardJson {
+                input: request.input,
+            },
+        };
+        let result =
+            solidity_standard_json::verify(self.client.clone(), verification_request).await;
+
+        match result {
+            Ok(source) => {
+                let response = VerifyResponseWrapper::ok(source);
+                Ok(tonic::Response::new(response.into()))
+            }
+            Err(Error::VerificationFailed { message }) => {
+                let response = VerifyResponseWrapper::err(message);
+                Ok(tonic::Response::new(response.into()))
+            }
+            Err(Error::InvalidArgument(message)) => Err(tonic::Status::invalid_argument(message)),
+            Err(Error::Internal(message)) => Err(tonic::Status::internal(message.to_string())),
+        }
     }
 
     async fn list_compiler_versions(
