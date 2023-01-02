8a102cdc7a9b84f34c3d7c0cb63ba1cf109d082b
Rim Rakhimov <rimrakhimov@gmail.com>
2023-01-02 15:38:13 +0300

    Disable metrics initialization when metrics should be disabled

--- libs/blockscout-service-launcher/src/launch.rs
+++ libs/blockscout-service-launcher/src/launch.rs
@@ -25,14 +25,21 @@ where
     R: HttpRouter + Send + Sync + Clone + 'static,
 {
     init_logs(&settings.service_name, &settings.tracing, &settings.jaeger);
-    let metrics = Metrics::new(&settings.service_name, &settings.metrics.route);
+
+    let metrics = settings
+        .metrics
+        .enabled
+        .then(|| Metrics::new(&settings.service_name, &settings.metrics.route));
 
     let mut futures = vec![];
+
     if settings.server.http.enabled {
         let http_server = {
             let http_server_future = http_serve(
                 http,
-                metrics.http_middleware().clone(),
+                metrics
+                    .as_ref()
+                    .map(|metrics| metrics.http_middleware().clone()),
                 settings.server.http.addr,
             );
             tokio::spawn(async move { http_server_future.await.map_err(anyhow::Error::msg) })
@@ -48,12 +55,12 @@ where
         futures.push(grpc_server)
     }
 
-    if settings.metrics.enabled {
+    if let Some(metrics) = metrics {
         let addr = settings.metrics.addr;
         futures.push(tokio::spawn(async move {
             metrics.run_server(addr).await?;
             Ok(())
-        }))
+        }));
     }
 
     let (res, _, others) = futures::future::select_all(futures).await;
@@ -63,20 +70,31 @@ where
     res?
 }
 
-fn http_serve<R>(http: R, metrics: PrometheusMetrics, addr: SocketAddr) -> actix_web::dev::Server
+fn http_serve<R>(
+    http: R,
+    metrics: Option<PrometheusMetrics>,
+    addr: SocketAddr,
+) -> actix_web::dev::Server
 where
     R: HttpRouter + Send + Sync + Clone + 'static,
 {
     tracing::info!("starting http server on addr {}", addr);
-    let server = HttpServer::new(move || {
-        App::new()
-            .wrap(metrics.clone())
-            .configure(configure_router(&http))
-    })
-    .bind(addr)
-    .unwrap_or_else(|_| panic!("failed to bind server"));
 
-    server.run()
+    if let Some(metrics) = metrics {
+        HttpServer::new(move || {
+            App::new()
+                .wrap(metrics.clone())
+                .configure(configure_router(&http))
+        })
+        .bind(addr)
+        .expect("failed to bind server")
+        .run()
+    } else {
+        HttpServer::new(move || App::new().configure(configure_router(&http)))
+            .bind(addr)
+            .expect("failed to bind server")
+            .run()
+    }
 }
 
 fn grpc_serve(
