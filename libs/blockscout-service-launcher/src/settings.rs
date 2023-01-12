3b9deea4f1bacf0be1a94df715f472409d08486c
Lymarenko Lev <lymarenko.lev@gmail.com>
2023-01-12 18:35:28 +0300

    Add json logs

--- libs/blockscout-service-launcher/src/settings.rs
+++ libs/blockscout-service-launcher/src/settings.rs
@@ -59,16 +59,27 @@ impl Default for MetricsSettings {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(rename_all = "snake_case")]
+pub enum TracingFormat {
+    Default,
+    Json,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(default, deny_unknown_fields)]
 pub struct TracingSettings {
     /// If disabled, tracing is not initialized for neither
     /// stdout, nor jaeger (enabled by default).
     pub enabled: bool,
+    pub format: TracingFormat,
 }
 
 impl Default for TracingSettings {
     fn default() -> Self {
-        Self { enabled: true }
+        Self {
+            enabled: true,
+            format: TracingFormat::Default,
+        }
     }
 }
 
