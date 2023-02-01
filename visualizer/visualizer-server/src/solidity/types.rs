b3177967fe7cbbd51ab8f77f60a540dd0aeba427
Lymarenko Lev <lymarenko.lev@gmail.com>
2023-02-01 18:29:42 +0300

    Add patch for filename paths

--- visualizer/visualizer-server/src/solidity/types.rs
+++ visualizer/visualizer-server/src/solidity/types.rs
@@ -7,7 +7,7 @@ use crate::proto::{
 use bytes::Bytes;
 use std::{
     collections::{BTreeMap, HashMap, HashSet},
-    path::PathBuf,
+    path::{Component, Path, PathBuf},
 };
 use visualizer::{OutputMask, ResponseFieldMask};
 
@@ -35,12 +35,46 @@ fn output_mask(field_mask: Option<FieldMask>) -> Result<OutputMask, anyhow::Erro
     Ok(output_mask)
 }
 
+fn fix_sources_paths(sources: BTreeMap<PathBuf, String>) -> BTreeMap<PathBuf, String> {
+    const REPLACE_PREFIX_FROM: &str = "@";
+    const REPLACE_PREFIX_TO: &str = "_";
+
+    let path_prefixes = sources
+        .keys()
+        .map(|path| {
+            path.components()
+                .find(|c| matches!(c, Component::Normal(_)))
+                .and_then(|c| c.as_os_str().to_str())
+                .unwrap_or_default()
+                .to_string()
+        })
+        .filter(|c| c.starts_with(REPLACE_PREFIX_TO))
+        .collect::<HashSet<_>>();
+
+    sources
+        .into_iter()
+        .map(|(path, content)| {
+            // remove / prefix
+            let mut path = path.strip_prefix("/").unwrap_or(&path).to_owned();
+            // if there is a similar prefix that starting with REPLACE_PREFIX_TO,
+            // then change our path to this prefix
+            for prefix in path_prefixes.iter() {
+                let prefix_to_replace = prefix.replacen(REPLACE_PREFIX_TO, REPLACE_PREFIX_FROM, 1);
+                if let Ok(rest) = path.strip_prefix(prefix_to_replace) {
+                    path = Path::new(prefix).join(rest);
+                }
+            }
+            (path, content)
+        })
+        .collect()
+}
+
 impl TryFrom<VisualizeContractsRequest> for visualizer::VisualizeContractsRequest {
     type Error = anyhow::Error;
 
     fn try_from(request: VisualizeContractsRequest) -> Result<Self, Self::Error> {
         Ok(Self {
-            sources: sources(request.sources),
+            sources: fix_sources_paths(sources(request.sources)),
             output_mask: output_mask(request.output_mask)?,
         })
     }
@@ -51,7 +85,7 @@ impl TryFrom<VisualizeStorageRequest> for visualizer::VisualizeStorageRequest {
 
     fn try_from(request: VisualizeStorageRequest) -> Result<Self, Self::Error> {
         Ok(Self {
-            sources: sources(request.sources),
+            sources: fix_sources_paths(sources(request.sources)),
             file_path: PathBuf::from(request.file_name),
             contract_name: request.contract_name,
             output_mask: output_mask(request.output_mask)?,
@@ -143,6 +177,73 @@ mod tests {
             ]
         }"#,
             "invalid response filed mask: abcd",
+        );
+    }
+
+    fn test_sources_paths(input: serde_json::Value, expected: serde_json::Value) {
+        let actual =
+            fix_sources_paths(serde_json::from_value(input).expect("invalid input: not map"));
+        assert_eq!(
+            serde_json::to_value(actual).expect("BTree map should be valud Value"),
+            expected
+        );
+    }
+
+    #[test]
+    fn valid_fix_sources_paths() {
+        test_sources_paths(
+            serde_json::json!({
+                "/root/kek/a.txt": "content",
+                "root/kek/b.txt": "content",
+                "/a.txt": "content",
+            }),
+            serde_json::json!({
+                "root/kek/a.txt": "content",
+                "root/kek/b.txt": "content",
+                "a.txt": "content",
+            }),
+        );
+
+        test_sources_paths(
+            serde_json::json!({
+                "@hello/kitty/a.sol": "content",
+                "/_hello/kitty/b.sol": "content",
+            }),
+            serde_json::json!({
+                "_hello/kitty/a.sol": "content",
+                "_hello/kitty/b.sol": "content",
+            }),
+        );
+
+        test_sources_paths(
+            serde_json::json!({
+                "/@hello/kitty/a.sol": "content",
+                "/_hello/kitty/b.sol": "content",
+            }),
+            serde_json::json!({
+                "_hello/kitty/a.sol": "content",
+                "_hello/kitty/b.sol": "content",
+            }),
+        );
+
+        test_sources_paths(
+            serde_json::json!({
+                "/h@llo/kitty/a.sol": "content",
+                "/h_llo/kitty/b.sol": "content",
+            }),
+            serde_json::json!({
+                "h@llo/kitty/a.sol": "content",
+                "h_llo/kitty/b.sol": "content",
+            }),
+        );
+
+        test_sources_paths(
+            serde_json::json!({
+                "/@hello/kitty/a.sol": "content",
+            }),
+            serde_json::json!({
+                "@hello/kitty/a.sol": "content",
+            }),
         );
     }
 }
