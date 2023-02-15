4ee08385940e223013d8f74c46fffa15d0539e18
Iskandarov Lev <iskandarov11@inbox.ru>
2023-02-15 16:26:15 +0400

    native coin holders growth (#359)

--- stats/stats/src/charts/lines/mod.rs
+++ stats/stats/src/charts/lines/mod.rs
@@ -1,4 +1,3 @@
-// mod lines_list;
 mod mock;
 
 mod accounts_growth;
@@ -8,7 +7,7 @@ mod average_gas_limit;
 mod average_gas_price;
 // mod average_txn_fee;
 mod gas_used_growth;
-// mod native_coin_holders_growth;
+mod native_coin_holders_growth;
 mod native_coin_supply;
 mod new_blocks;
 mod new_native_coins_transfers;
@@ -23,8 +22,8 @@ pub use average_gas_limit::AverageGasLimit;
 pub use average_gas_price::AverageGasPrice;
 // pub use average_txn_fee::AverageTxnFee;
 pub use gas_used_growth::GasUsedGrowth;
-// pub use native_coin_holders_growth::NativeCoinHoldersGrowth;
 pub use mock::MockLine;
+pub use native_coin_holders_growth::NativeCoinHoldersGrowth;
 pub use native_coin_supply::NativeCoinSupply;
 pub use new_blocks::NewBlocks;
 pub use new_native_coins_transfers::NewNativeCoinTransfers;
