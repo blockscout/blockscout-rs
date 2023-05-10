39bb84e6e3563303b806c949c8402fdb6322a871
Rim Rakhimov <rimrakhimov@gmail.com>
2023-05-10 20:05:37 +0400

    Update migration with indexes to add length index

--- eth-bytecode-db/eth-bytecode-db/migration/src/m20230510_151046_add_indexes_on_parts_sources_to_search_speedup.rs
+++ eth-bytecode-db/eth-bytecode-db/migration/src/m20230510_151046_add_indexes_on_parts_sources_to_search_speedup.rs
@@ -7,18 +7,24 @@ pub struct Migration;
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         let sql = r#"
-            CREATE INDEX IF NOT EXISTS "idx_sources_raw_creation_input_text_prefix" ON "sources" USING btree (LEFT("raw_creation_input_text", 500) text_pattern_ops);
-            CREATE INDEX IF NOT EXISTS "idx_sources_rraw_deployed_bytecode_text_prefix" ON "sources" USING btree (LEFT("raw_deployed_bytecode_text", 500) text_pattern_ops);
-            CREATE INDEX IF NOT EXISTS "idx_parts_data_text_prefix" ON "parts" USING btree (LEFT("data_text", 500) text_pattern_ops);
+            CREATE INDEX IF NOT EXISTS "idx_sources_raw_creation_input_text_prefix" ON "sources" (LEFT("raw_creation_input_text", 500) text_pattern_ops);
+            CREATE INDEX IF NOT EXISTS "idx_sources_raw_creation_input_text_length" ON "sources" (LENGTH("raw_creation_input_text"));
+            CREATE INDEX IF NOT EXISTS "idx_sources_raw_deployed_bytecode_text_prefix" ON "sources" (LEFT("raw_deployed_bytecode_text", 500) text_pattern_ops);
+            CREATE INDEX IF NOT EXISTS "idx_sources_raw_deployed_bytecode_text_length" ON "sources" (LENGTH("raw_deployed_bytecode_text"));
+            CREATE INDEX IF NOT EXISTS "idx_parts_data_text_prefix" ON "parts" (LENGTH("data_text"));
+            CREATE INDEX IF NOT EXISTS "idx_parts_data_text_length" ON "parts" (LEFT("data_text", 500) text_pattern_ops);
         "#;
         crate::from_sql(manager, sql).await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         let sql = r#"
-        DROP INDEX IF EXISTS "idx_sources_raw_creation_input_text_prefix";
-        DROP INDEX IF EXISTS "idx_sources_rraw_deployed_bytecode_text_prefix";
-        DROP INDEX IF EXISTS "idx_parts_data_text_prefix";
+            DROP INDEX IF EXISTS "idx_sources_raw_creation_input_text_prefix";
+            DROP INDEX IF EXISTS "idx_sources_raw_creation_input_text_length";
+            DROP INDEX IF EXISTS "idx_sources_raw_deployed_bytecode_text_prefix";
+            DROP INDEX IF EXISTS "idx_sources_raw_deployed_bytecode_text_length";
+            DROP INDEX IF EXISTS "idx_parts_data_text_prefix";
+            DROP INDEX IF EXISTS "idx_parts_data_text_length";
         "#;
         crate::from_sql(manager, sql).await
     }
