ee6913ae9024cace4c873c925fe7722db245a7df
Rim Rakhimov <rimrakhimov@gmail.com>
2023-05-10 20:26:06 +0400

    Update migration file

--- eth-bytecode-db/eth-bytecode-db/migration/src/m20230510_151046_add_indexes_on_parts_sources_to_search_speedup.rs
+++ eth-bytecode-db/eth-bytecode-db/migration/src/m20230510_151046_add_indexes_on_parts_sources_to_search_speedup.rs
@@ -11,8 +11,8 @@ impl MigrationTrait for Migration {
             CREATE INDEX IF NOT EXISTS "idx_sources_raw_creation_input_text_length" ON "sources" (LENGTH("raw_creation_input_text"));
             CREATE INDEX IF NOT EXISTS "idx_sources_raw_deployed_bytecode_text_prefix" ON "sources" (LEFT("raw_deployed_bytecode_text", 500) text_pattern_ops);
             CREATE INDEX IF NOT EXISTS "idx_sources_raw_deployed_bytecode_text_length" ON "sources" (LENGTH("raw_deployed_bytecode_text"));
-            CREATE INDEX IF NOT EXISTS "idx_parts_data_text_prefix" ON "parts" (LENGTH("data_text"));
-            CREATE INDEX IF NOT EXISTS "idx_parts_data_text_length" ON "parts" (LEFT("data_text", 500) text_pattern_ops);
+            CREATE INDEX IF NOT EXISTS "idx_parts_data_text_prefix" ON "parts" (LEFT("data_text", 500) text_pattern_ops);
+            CREATE INDEX IF NOT EXISTS "idx_parts_data_text_length" ON "parts" (LENGTH("data_text"));
         "#;
         crate::from_sql(manager, sql).await
     }
