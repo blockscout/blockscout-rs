f68b083fef11be78183aaea4455a08dedb6373c2
Rim Rakhimov <rimrakhimov@gmail.com>
2023-05-11 11:13:43 +0400

    Eliminate full_match search. Integrate match type retrieval into partial_match search (#465)

--- eth-bytecode-db/eth-bytecode-db/src/search/any_match.rs
+++ eth-bytecode-db/eth-bytecode-db/src/search/any_match.rs
@@ -1,6 +1,4 @@
-use super::{
-    find_full_match_contract, find_partial_match_contracts, BytecodeRemote, MatchContract,
-};
+use super::{find_partial_match_contracts, BytecodeRemote, MatchContract};
 use sea_orm::ConnectionTrait;
 
 pub async fn find_contract<C>(
@@ -10,10 +8,10 @@ pub async fn find_contract<C>(
 where
     C: ConnectionTrait,
 {
-    let full_matches = find_full_match_contract(db, remote).await?;
-    if !full_matches.is_empty() {
-        return Ok(full_matches);
-    };
+    // let full_matches = find_full_match_contract(db, remote).await?;
+    // if !full_matches.is_empty() {
+    //     return Ok(full_matches);
+    // };
 
     let partial_matches = find_partial_match_contracts(db, remote).await?;
     if !partial_matches.is_empty() {
--- eth-bytecode-db/eth-bytecode-db/src/search/candidates.rs
+++ eth-bytecode-db/eth-bytecode-db/src/search/candidates.rs
@@ -15,7 +15,7 @@ pub struct BytecodeCandidate {
 impl BytecodeCandidate {
     /// Compare self with remote bytecode.
     /// Return Ok(()) if this candidate meets the requirements
-    pub fn is_match(&self, remote_data: &Bytes) -> Result<(), CompareError> {
+    pub fn is_match(&self, remote_data: &Bytes) -> Result<MatchType, CompareError> {
         let local =
             LocalBytecode::new(&self.parts).expect("local bytecode should contain valid metadata");
         let result = compare(remote_data, &local);
@@ -36,17 +36,23 @@ where
 {
     let filtered_bytecodes: Vec<_> = candidates
         .into_iter()
-        .filter(|c| c.is_match(&remote.data).is_ok())
+        .filter_map(|c| {
+            c.is_match(&remote.data)
+                .ok()
+                .map(|match_type| (c, match_type))
+        })
         .collect();
     if !filtered_bytecodes.is_empty() {
-        let ids: Vec<i64> = filtered_bytecodes.iter().map(|b| b.bytecode.id).collect();
+        let ids: Vec<i64> = filtered_bytecodes
+            .iter()
+            .map(|(b, _)| b.bytecode.id)
+            .collect();
         tracing::debug!(ids = ?ids, "found filtered bytecodes");
     }
     let mut matches = vec![];
-    for bytecode in filtered_bytecodes.iter() {
+    for (bytecode, match_type) in filtered_bytecodes.iter() {
         let contract_match =
-            MatchContract::build(db, bytecode.bytecode.source_id, remote, MatchType::Partial)
-                .await?;
+            MatchContract::build(db, bytecode.bytecode.source_id, remote, *match_type).await?;
         matches.push(contract_match);
     }
     Ok(matches)
