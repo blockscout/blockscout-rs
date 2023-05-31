328f7c2ee226b43d80e2e407896464cd3598892e
Rim Rakhimov <rimrakhimov@gmail.com>
2023-05-31 23:15:31 +0400

    Update 'idx_parts_data_text_prefix' index to use 150 chars instead of 500

--- eth-bytecode-db/eth-bytecode-db/migration/src/lib.rs
+++ eth-bytecode-db/eth-bytecode-db/migration/src/lib.rs
@@ -13,6 +13,7 @@ mod m20230509_103951_add_trigger_parts_convert_data_to_text_value;
 mod m20230509_123604_duplicate_parts_existing_data_to_text_column;
 mod m20230509_132647_add_not_null_constraint_parts_data_text_column;
 mod m20230510_151046_add_search_speedup_indexes_on_parts;
+mod m20230531_191321_update_parts_data_text_prefix_index_to_150_chars;
 
 pub struct Migrator;
 
@@ -32,6 +33,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20230509_123604_duplicate_parts_existing_data_to_text_column::Migration),
             Box::new(m20230509_132647_add_not_null_constraint_parts_data_text_column::Migration),
             Box::new(m20230510_151046_add_search_speedup_indexes_on_parts::Migration),
+            Box::new(m20230531_191321_update_parts_data_text_prefix_index_to_150_chars::Migration),
         ]
     }
 }
