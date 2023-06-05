9db50e25a34b42c32c9e39e067cb4131b58d920c
Rim Rakhimov <rimrakhimov@gmail.com>
2023-06-05 20:12:27 +0400

    Remove unnecessary import

--- smart-contract-verifier/smart-contract-verifier-server/src/services/vyper_verifier.rs
+++ smart-contract-verifier/smart-contract-verifier-server/src/services/vyper_verifier.rs
@@ -11,7 +11,6 @@ use crate::{
 use smart_contract_verifier::{
     vyper, Compilers, ListFetcher, VerificationError, VyperClient, VyperCompiler,
 };
-use smart_contract_verifier_proto::blockscout::smart_contract_verifier::v2::VerifyVyperStandardJsonRequest;
 use std::sync::Arc;
 use tokio::sync::Semaphore;
 use tonic::{Request, Response, Status};
