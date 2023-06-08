505c07d1c5523638adbcb227693174db635fe561
Rim Rakhimov <rimrakhimov@gmail.com>
2023-06-09 02:41:13 +0400

    Fix returning a bad request for invalid standard json input, and returning a failure for actual bad request for the vyper verification

--- smart-contract-verifier/smart-contract-verifier-server/src/types/errors.rs
+++ smart-contract-verifier/smart-contract-verifier-server/src/types/errors.rs
@@ -1,7 +1,7 @@
 use thiserror::Error;
 #[derive(Error, Debug)]
 pub enum StandardJsonParseError {
-    #[error("content is not valid standard json: {0}")]
+    #[error("content is not a valid standard json: {0}")]
     InvalidContent(#[from] serde_json::Error),
     #[error("{0}")]
     BadRequest(#[from] anyhow::Error),
