91b307c122b4671b0cfc2baa9be5239aaaf2cc6c
Rim Rakhimov <rimrakhimov@gmail.com>
2023-09-27 22:29:22 +0400

    Fix tests

--- eth-bytecode-db/eth-bytecode-db-server/src/types/source.rs
+++ eth-bytecode-db/eth-bytecode-db-server/src/types/source.rs
@@ -228,37 +228,37 @@ mod tests {
             "files": [
                 {
                     "name": "immutable-references.json",
-                    "path": "/home/data/repository/contracts/full_match/5/0xb5fa8e1E33df0742Fa40F7bB5f41e782eb5524c3/immutable-references.json",
+                    "path": "/data/repository/contracts/full_match/5/0xb5fa8e1E33df0742Fa40F7bB5f41e782eb5524c3/immutable-references.json",
                     "content": "{\"76\":[{\"length\":32,\"start\":242}],\"90\":[{\"length\":32,\"start\":636}],\"92\":[{\"length\":32,\"start\":708}]}"
                 },
                 {
                     "name": "library-map.json",
-                    "path": "/home/data/repository/contracts/full_match/5/0xb5fa8e1E33df0742Fa40F7bB5f41e782eb5524c3/library-map.json",
+                    "path": "/data/repository/contracts/full_match/5/0xb5fa8e1E33df0742Fa40F7bB5f41e782eb5524c3/library-map.json",
                     "content": "{\"__$b01ade520cf6862e0b214d4ce779d49f2a$__\":\"77479d54e233b4b79b9e3f4cf2bd20575fdeb1bb\",\"__$21bb6923d223bd045c4cab9806bdf3594d$__\":\"4ea76bb37c82f6f453c4cbb4ae726036a7a8b820\"}"
                 },
                 {
                     "name": "metadata.json",
-                    "path": "/home/data/repository/contracts/full_match/5/0xb5fa8e1E33df0742Fa40F7bB5f41e782eb5524c3/metadata.json",
+                    "path": "/data/repository/contracts/full_match/5/0xb5fa8e1E33df0742Fa40F7bB5f41e782eb5524c3/metadata.json",
                     "content": "{\"compiler\":{\"version\":\"0.8.7+commit.e28d00a7\"},\"language\":\"Solidity\",\"output\":{\"abi\":[{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"_c\",\"type\":\"uint256\"}],\"stateMutability\":\"nonpayable\",\"type\":\"constructor\"},{\"inputs\":[],\"name\":\"a\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"name\":\"arr\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"b\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"c\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[],\"name\":\"test_array\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}],\"devdoc\":{\"kind\":\"dev\",\"methods\":{},\"version\":1},\"userdoc\":{\"kind\":\"user\",\"methods\":{},\"version\":1}},\"settings\":{\"compilationTarget\":{\"contracts/C2.sol\":\"C2\"},\"evmVersion\":\"london\",\"libraries\":{},\"metadata\":{\"bytecodeHash\":\"ipfs\"},\"optimizer\":{\"enabled\":false,\"runs\":200},\"remappings\":[]},\"sources\":{\"contracts/Array.sol\":{\"keccak256\":\"0xa3c043d47c03251f392e0dcdf7c2ad5069436cc72561b2404a195d84a78fb33c\",\"license\":\"MIT\",\"urls\":[\"bzz-raw://9eeae2fe8bb3e5addd48dea0b8c5a81581b4137ad605b103b7edc35ea1ab2997\",\"dweb:/ipfs/QmRaC1FTQxkcWwmyZeZ4twQswdpcTnN5GQJ2Db7RMdf14j\"]},\"contracts/Array2.sol\":{\"keccak256\":\"0x6b5b4acc9b4f10f7df803702516752978c40cb4eadd48a218dcc0503fd7f4f88\",\"license\":\"MIT\",\"urls\":[\"bzz-raw://6f9b39fbd016e6b0c62cb3a7d8abc8981fe383c345381dfd7533a8cb8a1ded69\",\"dweb:/ipfs/QmUznGnpnz1TFuQZzjDX4NzoVFsiwRzaA2T7gZQZUoFDim\"]},\"contracts/C1.sol\":{\"keccak256\":\"0x92ca0ef5999e6bae5556c90314c3b7fe3bf95cabf8423d8415ac3d3955f524df\",\"license\":\"MIT\",\"urls\":[\"bzz-raw://1bb5f1bc39e5507da05e9d7700546f74896b47c4da076fbd744d8b6269c32a09\",\"dweb:/ipfs/QmRoabfGHHmoKTPb8vtdWFuaesoc4mUjzuSgEDE8TpeYwF\"]},\"contracts/C2.sol\":{\"keccak256\":\"0x20dd630583b88340fae8cda0a6008c0db058ee4b35d8a1f5a44bcd66ef892782\",\"license\":\"MIT\",\"urls\":[\"bzz-raw://998e0420f165f61fec241b5fbd71f06bc150098c8981f0aeedbf5a95c240b2a0\",\"dweb:/ipfs/QmSG3mwV4zqk4NacHtf3MWSAbWW96sorEVjNBPJiWXs2LQ\"]}},\"version\":1}"
                 },
                 {
                     "name": "Array.sol",
-                    "path": "/home/data/repository/contracts/full_match/5/0xb5fa8e1E33df0742Fa40F7bB5f41e782eb5524c3/sources/contracts/Array.sol",
+                    "path": "/data/repository/contracts/full_match/5/0xb5fa8e1E33df0742Fa40F7bB5f41e782eb5524c3/sources/contracts/Array.sol",
                     "content": "// SPDX-License-Identifier: MIT\npragma solidity 0.8.7;\n\n// Array function to delete element at index and re-organize the array\n// so that there are no gaps between the elements.\nlibrary Array {\n    function remove(uint[] storage arr, uint index) public {\n        // Move the last element into the place to delete\n        require(arr.length > 0, \"Can't remove from empty array\");\n        arr[index] = arr[arr.length - 1];\n        arr.pop();\n    }\n}\n"
                 },
                 {
                     "name": "Array2.sol",
-                    "path": "/home/data/repository/contracts/full_match/5/0xb5fa8e1E33df0742Fa40F7bB5f41e782eb5524c3/sources/contracts/Array2.sol",
+                    "path": "/data/repository/contracts/full_match/5/0xb5fa8e1E33df0742Fa40F7bB5f41e782eb5524c3/sources/contracts/Array2.sol",
                     "content": "// SPDX-License-Identifier: MIT\npragma solidity 0.8.7;\n\n// Array function to delete element at index and re-organize the array\n// so that there are no gaps between the elements.\nlibrary Array2 {\n    function remove(uint[] storage arr, uint index) public {\n        // Move the last element into the place to delete\n        require(arr.length > 0, \"Can't remove from empty array\");\n        arr[index] = arr[arr.length - 1];\n        arr.pop();\n    }\n}\n"
                 },
                 {
                     "name": "C1.sol",
-                    "path": "/home/data/repository/contracts/full_match/5/0xb5fa8e1E33df0742Fa40F7bB5f41e782eb5524c3/sources/contracts/C1.sol",
+                    "path": "/data/repository/contracts/full_match/5/0xb5fa8e1E33df0742Fa40F7bB5f41e782eb5524c3/sources/contracts/C1.sol",
                     "content": "// SPDX-License-Identifier: MIT\npragma solidity 0.8.7;\n\ncontract C1 {\n    uint256 immutable public a = 0;\n}\n\n"
                 },
                 {
                     "name": "C2.sol",
-                    "path": "/home/data/repository/contracts/full_match/5/0xb5fa8e1E33df0742Fa40F7bB5f41e782eb5524c3/sources/contracts/C2.sol",
+                    "path": "/data/repository/contracts/full_match/5/0xb5fa8e1E33df0742Fa40F7bB5f41e782eb5524c3/sources/contracts/C2.sol",
                     "content": "// SPDX-License-Identifier: MIT\npragma solidity 0.8.7;\n\nimport \"./C1.sol\";\nimport \"./Array.sol\";\nimport \"./Array2.sol\";\n\ncontract C2 is C1 {\n    uint[] public arr;\n\n    uint256 immutable public b = 10;\n    uint256 immutable public c;\n\n    constructor(uint256 _c) {\n        c = _c;\n    }\n\n    function test_array() public {\n        for (uint i = 0; i < 3; i++) {\n            arr.push(i);\n        }\n\n        Array.remove(arr, 1);\n        Array2.remove(arr, 1);\n\n        assert(arr.length == 1);\n        assert(arr[0] == 0);\n    }\n}\n"
                 }
             ]
