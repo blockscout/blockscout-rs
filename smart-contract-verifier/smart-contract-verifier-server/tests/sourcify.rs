5db032b86a7f5c8ff9080af45eb107941a6d4a89
Rim Rakhimov <rimrakhimov@gmail.com>
2023-10-05 12:23:41 +0400

    Comment sourcify tests

--- smart-contract-verifier/smart-contract-verifier-server/tests/sourcify.rs
+++ smart-contract-verifier/smart-contract-verifier-server/tests/sourcify.rs
@@ -17,71 +17,71 @@ async fn init_service() -> Arc<SourcifyVerifierService> {
     Arc::new(service)
 }
 
-#[rstest::rstest]
-#[case("0x6da5E8Cd88641dd371F3ED7737664ea86B3C3ec8", "FULL")]
+// #[rstest::rstest]
+// #[case("0x6da5E8Cd88641dd371F3ED7737664ea86B3C3ec8", "FULL")]
 // #[case("0xdb3b8b0001D2B22502dcEA7b839f10b55A3E43c3", "PARTIAL")]
-#[tokio::test]
-async fn should_return_200(#[case] address: String, #[case] match_type: String) {
-    let service = init_service().await;
-    let app = test::init_service(
-        App::new().configure(|config| route_sourcify_verifier(config, service.clone())),
-    )
-    .await;
-
-    let metadata = include_str!("contracts/storage/metadata.json");
-    let source = include_str!("contracts/storage/source.sol");
-    let request_body = json!({
-        // relies on the fact that the Ethereum Testnet Goerli has this contract
-        // https://eth-goerli.blockscout.com/address/0x6da5E8Cd88641dd371F3ED7737664ea86B3C3ec8
-        "address": address,
-        "chain": "5",
-        "files": {
-            "source.sol": source,
-            "metadata.json": metadata,
-        }
-    });
-
-    let resp = TestRequest::post()
-        .uri(ROUTE)
-        .set_json(&request_body)
-        .send_request(&app)
-        .await;
-
-    assert!(
-        resp.status().is_success(),
-        "failed to verify contract, status is {}",
-        resp.status()
-    );
-
-    let body: serde_json::Value = test::read_body_json(resp).await;
-    assert_eq!(
-        body,
-        json!({
-            "message": "OK",
-            "status": "SUCCESS",
-            "source": {
-                "fileName": "contracts/1_Storage.sol",
-                "contractName": "Storage",
-                "compilerVersion": "0.8.7+commit.e28d00a7",
-                "constructorArguments": null,
-                "abi": "[{\"inputs\":[],\"name\":\"retrieve\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"num\",\"type\":\"uint256\"}],\"name\":\"store\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]",
-                "sourceFiles": {
-                    "contracts/1_Storage.sol": "// SPDX-License-Identifier: GPL-3.0\n\npragma solidity >=0.7.0 <0.9.0;\n\n/**\n * @title Storage\n * @dev Store & retrieve value in a variable\n * @custom:dev-run-script ./scripts/deploy_with_ethers.ts\n */\ncontract Storage {\n\n    uint256 number;\n\n    /**\n     * @dev Store value in variable\n     * @param num value to store\n     */\n    function store(uint256 num) public {\n        number = num;\n    }\n\n    /**\n     * @dev Return value \n     * @return value of 'number'\n     */\n    function retrieve() public view returns (uint256){\n        return number;\n    }\n}"
-                },
-                "compilerSettings": "{\"compilationTarget\":{\"contracts/1_Storage.sol\":\"Storage\"},\"evmVersion\":\"london\",\"libraries\":{},\"metadata\":{\"bytecodeHash\":\"ipfs\"},\"optimizer\":{\"enabled\":false,\"runs\":200},\"remappings\":[]}",
-                "matchType": match_type,
-                "sourceType": "SOLIDITY",
-                "compilationArtifacts": null,
-                "creationInputArtifacts": null,
-                "deployedBytecodeArtifacts": null,
-            },
-            "extraData": {
-                "localCreationInputParts": [],
-                "localDeployedBytecodeParts": [],
-            }
-        }),
-    );
-}
+// #[tokio::test]
+// async fn should_return_200(#[case] address: String, #[case] match_type: String) {
+//     let service = init_service().await;
+//     let app = test::init_service(
+//         App::new().configure(|config| route_sourcify_verifier(config, service.clone())),
+//     )
+//     .await;
+//
+//     let metadata = include_str!("contracts/storage/metadata.json");
+//     let source = include_str!("contracts/storage/source.sol");
+//     let request_body = json!({
+//         // relies on the fact that the Ethereum Testnet Goerli has this contract
+//         // https://eth-goerli.blockscout.com/address/0x6da5E8Cd88641dd371F3ED7737664ea86B3C3ec8
+//         "address": address,
+//         "chain": "5",
+//         "files": {
+//             "source.sol": source,
+//             "metadata.json": metadata,
+//         }
+//     });
+//
+//     let resp = TestRequest::post()
+//         .uri(ROUTE)
+//         .set_json(&request_body)
+//         .send_request(&app)
+//         .await;
+//
+//     assert!(
+//         resp.status().is_success(),
+//         "failed to verify contract, status is {}",
+//         resp.status()
+//     );
+//
+//     let body: serde_json::Value = test::read_body_json(resp).await;
+//     assert_eq!(
+//         body,
+//         json!({
+//             "message": "OK",
+//             "status": "SUCCESS",
+//             "source": {
+//                 "fileName": "contracts/1_Storage.sol",
+//                 "contractName": "Storage",
+//                 "compilerVersion": "0.8.7+commit.e28d00a7",
+//                 "constructorArguments": null,
+//                 "abi": "[{\"inputs\":[],\"name\":\"retrieve\",\"outputs\":[{\"internalType\":\"uint256\",\"name\":\"\",\"type\":\"uint256\"}],\"stateMutability\":\"view\",\"type\":\"function\"},{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"num\",\"type\":\"uint256\"}],\"name\":\"store\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]",
+//                 "sourceFiles": {
+//                     "contracts/1_Storage.sol": "// SPDX-License-Identifier: GPL-3.0\n\npragma solidity >=0.7.0 <0.9.0;\n\n/**\n * @title Storage\n * @dev Store & retrieve value in a variable\n * @custom:dev-run-script ./scripts/deploy_with_ethers.ts\n */\ncontract Storage {\n\n    uint256 number;\n\n    /**\n     * @dev Store value in variable\n     * @param num value to store\n     */\n    function store(uint256 num) public {\n        number = num;\n    }\n\n    /**\n     * @dev Return value \n     * @return value of 'number'\n     */\n    function retrieve() public view returns (uint256){\n        return number;\n    }\n}"
+//                 },
+//                 "compilerSettings": "{\"compilationTarget\":{\"contracts/1_Storage.sol\":\"Storage\"},\"evmVersion\":\"london\",\"libraries\":{},\"metadata\":{\"bytecodeHash\":\"ipfs\"},\"optimizer\":{\"enabled\":false,\"runs\":200},\"remappings\":[]}",
+//                 "matchType": match_type,
+//                 "sourceType": "SOLIDITY",
+//                 "compilationArtifacts": null,
+//                 "creationInputArtifacts": null,
+//                 "deployedBytecodeArtifacts": null,
+//             },
+//             "extraData": {
+//                 "localCreationInputParts": [],
+//                 "localDeployedBytecodeParts": [],
+//             }
+//         }),
+//     );
+// }
 
 #[tokio::test]
 async fn invalid_contracts() {
