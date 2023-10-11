7e87f522325623295748732a480ea7b34a69861a
Leonid Tyurin <lenyatyurin162586@gmail.com>
2023-10-11 14:59:28 +0400

    Extract test_database module

--- libs/blockscout-service-launcher/src/test/mod.rs
+++
@@ -1,9 +0,0 @@
-mod server;
-
-#[cfg(feature = "database")]
-mod database;
-
-pub use server::{get_test_server_settings, init_server, send_request};
-
-#[cfg(feature = "database")]
-pub use database::TestDbGuard;
---
+++ libs/blockscout-service-launcher/src/test_server/mod.rs
@@ -0,0 +1,3 @@
+mod server;
+
+pub use server::{get_test_server_settings, init_server, send_request};
