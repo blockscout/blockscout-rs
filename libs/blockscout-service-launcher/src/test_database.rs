2af5711750742c8793d53e832e4596833ad2d86d
Leonid Tyurin <lenyatyurin162586@gmail.com>
2023-10-16 22:21:05 +0400

    Remove Drop & refactor

--- libs/blockscout-service-launcher/src/test_database.rs
+++ libs/blockscout-service-launcher/src/test_database.rs
@@ -1,37 +1,35 @@
 use crate::database::{
     ConnectionTrait, Database, DatabaseConnection, DbErr, MigratorTrait, Statement,
 };
-use anyhow::anyhow;
 use std::{ops::Deref, sync::Arc};
 
 #[derive(Clone, Debug)]
 pub struct TestDbGuard {
     conn_with_db: Arc<DatabaseConnection>,
+    conn_without_db: Arc<DatabaseConnection>,
     base_db_url: String,
     db_name: String,
-    should_drop: bool,
 }
 
 impl TestDbGuard {
     pub async fn new<Migrator: MigratorTrait>(db_name: &str) -> Self {
         let base_db_url = std::env::var("DATABASE_URL")
             .expect("Database url must be set to initialize a test database");
+        let conn_without_db = Database::connect(&base_db_url)
+            .await
+            .expect("Connection to postgres (without database) failed");
         // We use a hash, as the name itself may be quite long and be trimmed.
         let db_name = format!("_{:x}", keccak_hash::keccak(db_name));
         let mut guard = TestDbGuard {
             conn_with_db: Arc::new(DatabaseConnection::Disconnected),
+            conn_without_db: Arc::new(conn_without_db),
             base_db_url,
             db_name,
-            should_drop: true,
         };
 
-        guard.init_database::<Migrator>().await;
+        guard.init_database().await;
+        guard.run_migrations::<Migrator>().await;
         guard
-    }
-
-    pub fn without_drop(mut self) -> Self {
-        self.should_drop = false;
-        self
     }
 
     pub fn client(&self) -> Arc<DatabaseConnection> {
@@ -42,30 +40,31 @@ impl TestDbGuard {
         format!("{}/{}", self.base_db_url, self.db_name)
     }
 
-    async fn init_database<Migrator: MigratorTrait>(&mut self) {
+    async fn init_database(&mut self) {
         // Create database
-        let conn_without_db = Database::connect(&self.base_db_url)
-            .await
-            .expect("Connection to postgres (without database) failed");
-        Self::drop_database(&conn_without_db, &self.db_name)
-            .await
-            .expect("Database drop failed");
-        Self::create_database(&conn_without_db, &self.db_name)
-            .await
-            .expect("Database creation failed");
+        self.drop_database().await;
+        self.create_database().await;
 
-        // Migrate database
         let db_url = self.db_url();
         let conn_with_db = Database::connect(&db_url)
             .await
             .expect("Connection to postgres (with database) failed");
-        Self::run_migrations::<Migrator>(&conn_with_db)
-            .await
-            .expect("Database migration failed");
         self.conn_with_db = Arc::new(conn_with_db);
     }
 
-    async fn create_database(db: &DatabaseConnection, db_name: &str) -> Result<(), DbErr> {
+    pub async fn drop_database(&self) {
+        Self::drop_database_internal(&self.conn_without_db, &self.db_name)
+            .await
+            .expect("Database drop failed");
+    }
+
+    async fn create_database(&self) {
+        Self::create_database_internal(&self.conn_without_db, &self.db_name)
+            .await
+            .expect("Database creation failed");
+    }
+
+    async fn create_database_internal(db: &DatabaseConnection, db_name: &str) -> Result<(), DbErr> {
         tracing::info!(name = db_name, "creating database");
         db.execute(Statement::from_string(
             db.get_database_backend(),
@@ -75,7 +74,7 @@ impl TestDbGuard {
         Ok(())
     }
 
-    async fn drop_database(db: &DatabaseConnection, db_name: &str) -> Result<(), DbErr> {
+    async fn drop_database_internal(db: &DatabaseConnection, db_name: &str) -> Result<(), DbErr> {
         tracing::info!(name = db_name, "dropping database");
         db.execute(Statement::from_string(
             db.get_database_backend(),
@@ -85,8 +84,10 @@ impl TestDbGuard {
         Ok(())
     }
 
-    async fn run_migrations<Migrator: MigratorTrait>(db: &DatabaseConnection) -> Result<(), DbErr> {
-        Migrator::up(db, None).await
+    async fn run_migrations<Migrator: MigratorTrait>(&self) {
+        Migrator::up(self.conn_with_db.as_ref(), None)
+            .await
+            .expect("Database migration failed");
     }
 }
 
@@ -94,29 +95,5 @@ impl Deref for TestDbGuard {
     type Target = DatabaseConnection;
     fn deref(&self) -> &Self::Target {
         &self.conn_with_db
-    }
-}
-
-impl Drop for TestDbGuard {
-    fn drop(&mut self) {
-        if !self.should_drop {
-            return;
-        }
-
-        let base_db_url = self.base_db_url.clone();
-        let db_name = self.db_name.clone();
-
-        tokio::spawn(async move {
-            // Workaround for postgres error `cannot drop the currently open database`:
-            // We need to create another connection without the database to drop it
-            let conn_without_db = Database::connect(base_db_url.as_str())
-                .await
-                .map_err(|_| anyhow!("Failed to connect to the database"))
-                .unwrap();
-            TestDbGuard::drop_database(&conn_without_db, db_name.as_str())
-                .await
-                .map_err(|e| anyhow!("Failed to drop the database: {}", e))
-                .unwrap();
-        });
     }
 }
