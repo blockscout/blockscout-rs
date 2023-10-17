de2e809d6d7b0fc42cc8abfe54b3fa25305d73e3
Rim Rakhimov <rimrakhimov@gmail.com>
2023-10-17 15:20:03 +0300

    Update eth-bytecode-db-server initialization to use blockscout-service-launcher v0.9.0

--- eth-bytecode-db/eth-bytecode-db-server/src/settings.rs
+++ eth-bytecode-db/eth-bytecode-db-server/src/settings.rs
@@ -1,25 +1,10 @@
 use blockscout_service_launcher::{
-    launcher::{MetricsSettings, ServerSettings},
+    launcher::{ConfigSettings, MetricsSettings, ServerSettings},
     tracing::{JaegerSettings, TracingSettings},
 };
-use config::{Config, File};
-use serde::{de, Deserialize};
+use serde::Deserialize;
 use serde_with::{serde_as, DisplayFromStr};
 use std::collections::HashMap;
-
-/// Wrapper under [`serde::de::IgnoredAny`] which implements
-/// [`PartialEq`] and [`Eq`] for fields to be ignored.
-#[derive(Copy, Clone, Debug, Default, Deserialize)]
-struct IgnoredAny(de::IgnoredAny);
-
-impl PartialEq for IgnoredAny {
-    fn eq(&self, _other: &Self) -> bool {
-        // We ignore that values, so they should not impact the equality
-        true
-    }
-}
-
-impl Eq for IgnoredAny {}
 
 #[derive(Debug, Clone, Deserialize, PartialEq, Eq)]
 #[serde(deny_unknown_fields)]
@@ -42,12 +27,10 @@ pub struct Settings {
 
     #[serde(default)]
     pub authorized_keys: HashMap<String, ApiKey>,
+}
 
-    // Is required as we deny unknown fields, but allow users provide
-    // path to config through PREFIX__CONFIG env variable. If removed,
-    // the setup would fail with `unknown field `config`, expected one of...`
-    #[serde(default, rename = "config")]
-    config_path: IgnoredAny,
+impl ConfigSettings for Settings {
+    const SERVICE_NAME: &'static str = "ETH_BYTECODE_DB";
 }
 
 #[derive(Debug, Clone, Deserialize, PartialEq, Eq)]
@@ -99,22 +82,6 @@ pub struct ApiKey {
 }
 
 impl Settings {
-    pub fn new() -> anyhow::Result<Self> {
-        let config_path = std::env::var("ETH_BYTECODE_DB__CONFIG");
-
-        let mut builder = Config::builder();
-        if let Ok(config_path) = config_path {
-            builder = builder.add_source(File::with_name(&config_path));
-        };
-        // Use `__` so that it would be possible to address keys with underscores in names (e.g. `first_key`)
-        builder =
-            builder.add_source(config::Environment::with_prefix("ETH_BYTECODE_DB").separator("__"));
-
-        let settings: Settings = builder.build()?.try_deserialize()?;
-
-        Ok(settings)
-    }
-
     pub fn default(database_url: String, verifier_uri: tonic::transport::Uri) -> Self {
         Self {
             server: Default::default(),
@@ -130,7 +97,6 @@ impl Settings {
             sourcify: Default::default(),
             verifier_alliance_database: Default::default(),
             authorized_keys: Default::default(),
-            config_path: Default::default(),
         }
     }
 }
