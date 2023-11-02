8501491279da528ecf8f02b94f0feb18b2370733
Rim Rakhimov <rimrakhimov@gmail.com>
2023-11-02 11:25:04 +0400

    Add  contract_address and chain_id to the error messages on database insertion (#657)

--- eth-bytecode-db/eth-bytecode-db/src/verification/handlers/mod.rs
+++ eth-bytecode-db/eth-bytecode-db/src/verification/handlers/mod.rs
@@ -29,6 +29,42 @@ enum EthBytecodeDbAction<'a> {
     },
 }
 
+impl<'a> EthBytecodeDbAction<'a> {
+    fn contract_address(&self) -> Option<blockscout_display_bytes::Bytes> {
+        if let EthBytecodeDbAction::SaveData {
+            verification_metadata:
+                Some(VerificationMetadata {
+                    contract_address: Some(contract_address),
+                    ..
+                }),
+            ..
+        } = self
+        {
+            Some(blockscout_display_bytes::Bytes::from(
+                contract_address.to_vec(),
+            ))
+        } else {
+            None
+        }
+    }
+
+    fn chain_id(&self) -> Option<i64> {
+        if let EthBytecodeDbAction::SaveData {
+            verification_metadata:
+                Some(VerificationMetadata {
+                    chain_id: Some(chain_id),
+                    ..
+                }),
+            ..
+        } = self
+        {
+            Some(*chain_id)
+        } else {
+            None
+        }
+    }
+}
+
 enum VerifierAllianceDbAction<'a> {
     IgnoreDb,
     SaveIfDeploymentExists {
@@ -96,12 +132,41 @@ impl<'a> VerifierAllianceDbAction<'a> {
     }
 }
 
+impl<'a> VerifierAllianceDbAction<'a> {
+    fn contract_address(&self) -> Option<blockscout_display_bytes::Bytes> {
+        match self {
+            VerifierAllianceDbAction::IgnoreDb => None,
+            VerifierAllianceDbAction::SaveIfDeploymentExists {
+                contract_address, ..
+            } => Some(contract_address),
+            VerifierAllianceDbAction::SaveWithDeploymentData {
+                contract_address, ..
+            } => Some(contract_address),
+        }
+        .map(|contract_address| blockscout_display_bytes::Bytes::from(contract_address.to_vec()))
+    }
+
+    fn chain_id(&self) -> Option<i64> {
+        match self {
+            VerifierAllianceDbAction::IgnoreDb => None,
+            VerifierAllianceDbAction::SaveIfDeploymentExists { chain_id, .. } => Some(*chain_id),
+            VerifierAllianceDbAction::SaveWithDeploymentData { chain_id, .. } => Some(*chain_id),
+        }
+    }
+}
+
 async fn process_verify_response(
     response: smart_contract_verifier::VerifyResponse,
     eth_bytecode_db_action: EthBytecodeDbAction<'_>,
     alliance_db_action: VerifierAllianceDbAction<'_>,
 ) -> Result<Source, Error> {
     let source = from_response_to_source(response).await?;
+
+    let eth_bytecode_db_action_contract_address = eth_bytecode_db_action.contract_address();
+    let eth_bytecode_db_action_chain_id = eth_bytecode_db_action.chain_id();
+
+    let alliance_db_action_contract_address = alliance_db_action.contract_address();
+    let alliance_db_action_chain_id = alliance_db_action.chain_id();
 
     let process_eth_bytecode_db_future =
         process_eth_bytecode_db_action(source.clone(), eth_bytecode_db_action);
@@ -113,12 +178,16 @@ async fn process_verify_response(
     let (process_eth_bytecode_db_result, process_alliance_db_result) =
         futures::future::join(process_eth_bytecode_db_future, process_alliance_db_future).await;
     let _ = process_eth_bytecode_db_result.map_err(|err: anyhow::Error| {
-        println!("Error while inserting contract data into database: {err:#}");
-        tracing::error!("Error while inserting contract data into database: {err:#}")
+        tracing::error!(
+            ?eth_bytecode_db_action_contract_address,
+            ?eth_bytecode_db_action_chain_id,
+            "Error while inserting contract data into database: {err:#}"
+        )
     });
     let _ = process_alliance_db_result.map_err(|err: anyhow::Error| {
-        println!("Error while inserting contract data into verifier alliance database: {err:#}");
         tracing::error!(
+            ?alliance_db_action_contract_address,
+            ?alliance_db_action_chain_id,
             "Error while inserting contract data into verifier alliance database: {err:#}"
         )
     });
