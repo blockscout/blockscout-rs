c6a324c876be46ce474f79abd1f94d722a6d5d28
Leonid Tyurin <lenyatyurin162586@gmail.com>
2023-11-09 17:20:34 +0400

    Update launcher version

--- smart-contract-verifier/smart-contract-verifier-server/src/main.rs
+++ smart-contract-verifier/smart-contract-verifier-server/src/main.rs
@@ -1,7 +1,8 @@
+use blockscout_service_launcher::launcher::ConfigSettings;
 use smart_contract_verifier_server::Settings;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    let settings = Settings::new().expect("failed to read config");
+    let settings = Settings::build().expect("failed to read config");
     smart_contract_verifier_server::run(settings).await
 }
