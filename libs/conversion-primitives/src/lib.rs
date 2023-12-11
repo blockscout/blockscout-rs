aae6f2dc1da31ee3e94b28ba8b5ed10ac71d4f05
Rim Rakhimov <rimrakhimov@gmail.com>
2023-12-12 02:44:02 +0300

    Rename conversion-error crate to conversion-primitives

--- libs/conversion-primitives/src/lib.rs
+++ libs/conversion-primitives/src/lib.rs
@@ -1,6 +1,24 @@
 use thiserror::Error;
 
 #[derive(Error, Debug)]
+#[error("invalid argument: {0}")]
+pub struct InvalidArgument(String);
+impl InvalidArgument {
+    pub fn new(message: impl Into<String>) -> Self {
+        Self(message.into())
+    }
+}
+
+#[derive(Error, Debug)]
+#[error("internal error: {0}")]
+pub struct InternalError(String);
+impl InternalError {
+    pub fn new(message: impl Into<String>) -> Self {
+        Self(message.into())
+    }
+}
+
+#[derive(Error, Debug)]
 pub enum Error {
     #[error("invalid argument: {0}")]
     InvalidArgument(String),
@@ -27,6 +45,20 @@ cfg_if::cfg_if! {
             "one of the features ['tonic-0_8'] \
              must be enabled"
         );
+    }
+}
+
+#[cfg(feature = "tonic")]
+impl From<InvalidArgument> for tonic::Status {
+    fn from(value: InvalidArgument) -> Self {
+        tonic::Status::invalid_argument(value.0)
+    }
+}
+
+#[cfg(feature = "tonic")]
+impl From<InternalError> for tonic::Status {
+    fn from(value: InternalError) -> Self {
+        tonic::Status::internal(value.0)
     }
 }
 
