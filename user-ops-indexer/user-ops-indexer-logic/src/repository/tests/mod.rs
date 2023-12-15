384133d0b9ae83abc14ca55179cac92f8362bf5d
Kirill Fedoseev <kirill@blockscout.com>
2023-12-15 11:03:33 +0400

    fix: tests

--- user-ops-indexer/user-ops-indexer-logic/src/repository/tests/mod.rs
+++ user-ops-indexer/user-ops-indexer-logic/src/repository/tests/mod.rs
@@ -1,5 +1,8 @@
 use blockscout_service_launcher::test_database::TestDbGuard;
-use sea_orm::{ConnectionTrait, Database, DatabaseConnection};
+use migration::{
+    from_sql, Alias, DynIden, IntoIden, MigrationName, MigrationTrait, MigratorTrait, SchemaManager,
+};
+use sea_orm::{Database, DatabaseConnection, DbErr};
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
@@ -8,7 +11,7 @@ pub async fn get_shared_db() -> Arc<DatabaseConnection> {
     let mut initialized = SHARED_DB_MUTEX.lock().await;
     match initialized.clone() {
         None => {
-            let client = init_db("shared").await;
+            let client = TestDbGuard::new::<TestMigrator>("shared").await;
             *initialized = Some(client.db_url());
             client.client()
         }
@@ -16,16 +19,49 @@ pub async fn get_shared_db() -> Arc<DatabaseConnection> {
     }
 }
 
-pub async fn init_db(name: &str) -> TestDbGuard {
-    let db = TestDbGuard::new::<migration::Migrator>(name).await;
+pub struct TestMigrator;
 
-    db.execute_unprepared(include_str!("blockscout_tables.sql"))
-        .await
-        .unwrap();
+#[async_trait::async_trait]
+impl MigratorTrait for TestMigrator {
+    fn migrations() -> Vec<Box<dyn MigrationTrait>> {
+        let before: Vec<Box<dyn MigrationTrait>> = vec![Box::new(TestMigrationBefore)];
+        let after: Vec<Box<dyn MigrationTrait>> = vec![Box::new(TestMigrationAfter)];
+        before
+            .into_iter()
+            .chain(migration::Migrator::migrations())
+            .chain(after)
+            .collect()
+    }
+    fn migration_table_name() -> DynIden {
+        Alias::new("user_ops_indexer_migrations").into_iden()
+    }
+}
 
-    db.execute_unprepared(include_str!("fixtures.sql"))
-        .await
-        .unwrap();
+pub struct TestMigrationBefore;
+pub struct TestMigrationAfter;
 
-    db
+impl MigrationName for TestMigrationBefore {
+    fn name(&self) -> &str {
+        "test_migration_before_0"
+    }
+}
+
+impl MigrationName for TestMigrationAfter {
+    fn name(&self) -> &str {
+        "test_migration_after_0"
+    }
+}
+
+#[async_trait::async_trait]
+impl MigrationTrait for TestMigrationBefore {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        from_sql(manager, include_str!("blockscout_tables.sql")).await
+    }
+}
+
+#[async_trait::async_trait]
+impl MigrationTrait for TestMigrationAfter {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        from_sql(manager, include_str!("fixtures.sql")).await
+    }
 }
