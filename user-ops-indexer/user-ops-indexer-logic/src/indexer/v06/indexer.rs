89458669c614c4a6b967955aa83d766f991433d6
Kirill Fedoseev <kirill@blockscout.com>
2024-01-08 15:53:13 +0200

    fix: clippy

--- user-ops-indexer/user-ops-indexer-logic/src/indexer/v06/indexer.rs
+++ user-ops-indexer/user-ops-indexer-logic/src/indexer/v06/indexer.rs
@@ -284,7 +284,7 @@ impl<'a, C: PubsubClient> IndexerV06<'a, C> {
                             Ok(model) => Some(model),
                             Err(err) => {
                                 let logs_start_index =
-                                    logs.get(0).and_then(|l| l.log_index).map(|i| i.as_u64());
+                                    logs.first().and_then(|l| l.log_index).map(|i| i.as_u64());
                                 let logs_count = logs.len();
                                 tracing::error!(
                                     tx_hash = ?tx_hash,
