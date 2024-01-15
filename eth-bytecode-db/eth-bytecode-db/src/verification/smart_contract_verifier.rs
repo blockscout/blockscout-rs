66b52df02f5460df2718ea6716b6bce20c13d0c3
Rim Rakhimov <rimrakhimov@gmail.com>
2024-01-15 14:09:13 +0400

    Fix merge errors

--- eth-bytecode-db/eth-bytecode-db/src/verification/smart_contract_verifier.rs
+++ eth-bytecode-db/eth-bytecode-db/src/verification/smart_contract_verifier.rs
@@ -1,9 +1,7 @@
 pub use smart_contract_verifier_proto::blockscout::smart_contract_verifier::v2::{
-    solidity_verifier_client,
     source::{MatchType, SourceType},
-    sourcify_verifier_client,
     verify_response::{extra_data::BytecodePart, ExtraData, Status},
-    vyper_verifier_client, BytecodeType, ListCompilerVersionsRequest, Source, VerificationMetadata,
+    BytecodeType, ListCompilerVersionsRequest, Source, VerificationMetadata,
     VerifyFromEtherscanSourcifyRequest, VerifyResponse, VerifySolidityMultiPartRequest,
     VerifySolidityStandardJsonRequest, VerifySourcifyRequest, VerifyVyperMultiPartRequest,
     VerifyVyperStandardJsonRequest,
