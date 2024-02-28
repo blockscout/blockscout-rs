f42a713932fd1e3ec5f8d910d04885c4ea13335c
Rim Rakhimov <rimrakhimov@gmail.com>
2024-02-28 12:19:58 +0300

    Alliance verifier extractor setup

---
+++ eth-bytecode-db-extractors/alliance-migration/src/lib.rs
@@ -0,0 +1,32 @@
+pub use sea_orm_migration::prelude::*;
+use sea_orm_migration::sea_orm::{Statement, TransactionTrait};
+
+mod m20220101_000001_create_contract_addresses_table;
+
+pub struct Migrator;
+
+#[async_trait::async_trait]
+impl MigratorTrait for Migrator {
+    fn migrations() -> Vec<Box<dyn MigrationTrait>> {
+        vec![
+            Box::new(job_queue::migration::Migration),
+            Box::new(m20220101_000001_create_contract_addresses_table::Migration),
+        ]
+    }
+}
+
+pub async fn from_statements(
+    manager: &SchemaManager<'_>,
+    statements: &[&str],
+) -> Result<(), DbErr> {
+    let txn = manager.get_connection().begin().await?;
+    for statement in statements {
+        txn.execute(Statement::from_string(
+            manager.get_database_backend(),
+            statement.to_string(),
+        ))
+            .await
+            .map_err(|err| DbErr::Migration(format!("{err}\nQuery: {statement}")))?;
+    }
+    txn.commit().await
+}
---
+++ eth-bytecode-db-extractors/alliance-migration/src/m20220101_000001_create_contract_addresses_table.rs
@@ -0,0 +1,53 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let create_contract_addresses_table = r#"
+            CREATE TABLE "contract_addresses" (
+                "chain_id" numeric NOT NULL,
+                "address" bytea NOT NULL,
+
+                "compiler" varchar NOT NULL,
+                "version" varchar NOT NULL,
+                "language" varchar NOT NULL,
+
+                "sources" jsonb NOT NULL,
+                "compiler_settings" jsonb NOT NULL,
+
+                "_job_id" uuid NOT NULL REFERENCES "_job_queue" ("id"),
+
+                PRIMARY KEY ("chain_id", "address")
+            );
+        "#;
+
+        let create_job_queue_connection_statements =
+            job_queue::migration::create_job_queue_connection_statements("contract_addresses");
+
+        let mut statements = vec![create_contract_addresses_table];
+        statements.extend(
+            create_job_queue_connection_statements
+                .iter()
+                .map(|v| v.as_str()),
+        );
+
+        crate::from_statements(manager, &statements).await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let drop_job_queue_connection_statements =
+            job_queue::migration::drop_job_queue_connection_statements("contract_addresses");
+        let drop_table_contract_addresses = "DROP TABLE contract_addresses;";
+
+        let mut statements = drop_job_queue_connection_statements
+            .iter()
+            .map(|v| v.as_str())
+            .collect::<Vec<_>>();
+        statements.extend([drop_table_contract_addresses]);
+
+        crate::from_statements(manager, &statements).await
+    }
+}
