bb2dacf986a5eef6847783d4cbefcca677d25b6d
Kirill Ivanov <bragov4ik@hotmail.com>
2024-04-16 18:34:59 +0300

    common updater trait

--- stats/stats/src/charts/db_interaction/updater/mod.rs
+++ stats/stats/src/charts/db_interaction/updater/mod.rs
@@ -5,7 +5,7 @@
 use async_trait::async_trait;
 use sea_orm::prelude::*;
 
-use crate::{Chart, DateValue, UpdateError};
+use crate::{charts::find_chart, Chart, UpdateError};
 
 mod batch;
 pub(crate) mod common_operations;
@@ -17,3 +17,47 @@ pub use batch::ChartBatchUpdater;
 pub use dependent::{last_point, parse_and_cumsum, parse_and_sum, ChartDependentUpdater};
 pub use full::ChartFullUpdater;
 pub use partial::ChartPartialUpdater;
+
+#[async_trait]
+trait ChartUpdater: Chart {
+    /// Update only data (values) of the chart (`chart_data` table).
+    ///
+    /// Implementation is expected to be highly variable.
+    async fn update_values(
+        &self,
+        db: &DatabaseConnection,
+        blockscout: &DatabaseConnection,
+        force_full: bool,
+    ) -> Result<(), UpdateError>;
+
+    /// Update only metadata of the chart (`charts` table).
+    ///
+    /// Generally better to call after changing chart data to keep
+    /// the info relevant (i.e. if it depends on values).
+    async fn update_metadata(
+        &self,
+        db: &DatabaseConnection,
+        _blockscout: &DatabaseConnection,
+    ) -> Result<(), UpdateError> {
+        // todo: factor out to `update_chart`
+        let chart_id = find_chart(db, self.name())
+            .await
+            .map_err(UpdateError::StatsDB)?
+            .ok_or_else(|| UpdateError::NotFound(self.name().into()))?;
+        let time = chrono::Utc::now();
+        common_operations::update::set_last_updated_at(chart_id, db, time)
+            .await
+            .map_err(UpdateError::StatsDB)
+    }
+
+    /// Update data and metadata of the chart
+    async fn update_chart(
+        &self,
+        db: &DatabaseConnection,
+        blockscout: &DatabaseConnection,
+        force_full: bool,
+    ) -> Result<(), UpdateError> {
+        self.update_values(db, blockscout, force_full).await?;
+        self.update_metadata(db, blockscout).await
+    }
+}
