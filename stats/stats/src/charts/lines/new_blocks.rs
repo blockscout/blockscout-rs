f7af15ab17d3c0e0b424c68192ea3235a97de356
Kirill Ivanov <bragov4ik@hotmail.com>
2024-04-16 18:35:30 +0300

    fix test

--- stats/stats/src/charts/lines/new_blocks.rs
+++ stats/stats/src/charts/lines/new_blocks.rs
@@ -75,7 +75,7 @@ impl crate::Chart for NewBlocks {
 mod tests {
     use super::*;
     use crate::{
-        charts::db_interaction::updater::get_min_block_blockscout,
+        charts::db_interaction::updater::common_operations::read::get_min_block_blockscout,
         get_chart_data,
         tests::{init_db::init_db_all, mock_blockscout::fill_mock_blockscout_data},
         Chart, ExtendedDateValue,
