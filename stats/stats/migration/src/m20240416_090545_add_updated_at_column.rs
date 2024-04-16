49e4c4242e5b862b7f257120678ed12a6e3d8357
Kirill Ivanov <bragov4ik@hotmail.com>
2024-04-16 16:45:37 +0300

    change timestamp type in db to contain timezone

--- stats/stats/migration/src/m20240416_090545_add_updated_at_column.rs
+++ stats/stats/migration/src/m20240416_090545_add_updated_at_column.rs
@@ -6,6 +6,8 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // add & populate column
+        // fix timezone of timestamps (everywhere in the code UTC is used)
         let sql = r#"
             ALTER TABLE charts
                 ADD COLUMN last_updated_at timestamp;
@@ -13,15 +15,26 @@ impl MigrationTrait for Migration {
             UPDATE charts SET last_updated_at = (
                 SELECT max(date) FROM chart_data
                 WHERE charts.id = chart_id
-                GROUP BY chart_id
-            );"#;
+                GROUP BY chart_id);
+
+            ALTER TABLE charts
+                ALTER COLUMN created_at TYPE timestamptz USING created_at
+                , ALTER COLUMN created_at SET DEFAULT (now() at time zone 'utc');
+            ALTER TABLE chart_data
+                ALTER COLUMN created_at TYPE timestamptz USING created_at
+                , ALTER COLUMN created_at SET DEFAULT (now() at time zone 'utc');
+        "#;
         crate::from_sql(manager, sql).await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         let sql = r#"
             ALTER TABLE charts
-                DROP COLUMN last_updated_at;"#;
+                DROP COLUMN last_updated_at;
+
+            ALTER TABLE charts ALTER COLUMN created_at SET DEFAULT (now());
+            ALTER TABLE chart_data ALTER COLUMN created_at SET DEFAULT (now());
+        "#;
         crate::from_sql(manager, sql).await
     }
 }
--- stats/stats/src/charts/db_interaction/updater/common_operations/update.rs
+++ stats/stats/src/charts/db_interaction/updater/common_operations/update.rs
@@ -6,6 +6,7 @@ pub async fn set_last_updated_at<C>(
     // chart: &C,
     chart_id: i32,
     db: &DatabaseConnection,
+    at: chrono::NaiveDateTime,
 ) -> Result<(), DbErr>
 where
     C: Chart + ?Sized,
