5652a8ff9dbc4d827e5797319850fd604490c78c
Kirill Ivanov <bragov4ik@hotmail.com>
2024-04-17 21:02:36 +0300

    self-review

--- stats/stats-proto/build.rs
+++ stats/stats-proto/build.rs
@@ -17,7 +17,6 @@ fn compile(
         .protoc_arg("grpc_api_configuration=proto/api_config_http.yaml,output_format=yaml,allow_merge=true,merge_file_name=stats")
         .bytes(["."])
         .type_attribute(".", "#[actix_prost_macros::serde]")
-        // &bool -> bool
         .field_attribute(".blockscout.stats.v1.Point.is_approximate", "#[serde(skip_serializing_if = \"std::ops::Not::not\")]")
         .field_attribute(".blockscout.stats.v1.Point.is_approximate", "#[serde(default)]");
 
--- stats/stats-server/src/serializers.rs
+++ stats/stats-server/src/serializers.rs
@@ -2,10 +2,8 @@ use stats::ExtendedDateValue;
 use stats_proto::blockscout::stats::v1::Point;
 
 pub fn serialize_line_points(data: Vec<ExtendedDateValue>) -> Vec<Point> {
-    // let today: NaiveDate = serde_json::from_str(r#""2024-4-8""#).unwrap();
     data.into_iter()
         .map(|point| {
-            // let is_approximate = point.date == today;
             Point {
                 date: point.date.to_string(),
                 value: point.value,
