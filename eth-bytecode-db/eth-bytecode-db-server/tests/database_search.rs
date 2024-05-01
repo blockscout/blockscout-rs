485fc9e02f46d012c9fb6d5d04dae1680bbf90b7
Rim Rakhimov <rimrakhimov@gmail.com>
2024-05-01 09:43:47 +0300

    Cargo fmt

--- eth-bytecode-db/eth-bytecode-db-server/tests/database_search.rs
+++ eth-bytecode-db/eth-bytecode-db-server/tests/database_search.rs
@@ -375,7 +375,8 @@ async fn search_blueprint_contracts() {
 
     /********** Setup **********/
 
-    let mut test_data = test_input_data::basic(verification::SourceType::Solidity, MatchType::Partial);
+    let mut test_data =
+        test_input_data::basic(verification::SourceType::Solidity, MatchType::Partial);
     test_data.set_is_blueprint(true);
     test_data.set_bytecode(smart_contract_verifier_v2::verify_response::ExtraData {
         local_creation_input_parts: vec![
