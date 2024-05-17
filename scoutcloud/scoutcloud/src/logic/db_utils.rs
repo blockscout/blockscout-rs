7235b3105c1a4e2ab3af001d31b8c7a8193e855a
sevenzing <41516657+sevenzing@users.noreply.github.com>
2024-05-17 15:02:39 +0200

    Fix uuid_eq bug

--- scoutcloud/scoutcloud/src/logic/db_utils.rs
+++ scoutcloud/scoutcloud/src/logic/db_utils.rs
@@ -1,7 +1,9 @@
 #[macro_export]
 macro_rules! uuid_eq {
     ($col:expr, $val:expr) => {
-        sea_orm::prelude::Expr::col($col)
+        // NOTE: using entity_name() to get the table name
+        // source: https://github.com/SeaQL/sea-orm/blob/010433bf1633dd387f8493071c0b3838a7fbb575/src/entity/column.rs#L30
+        sea_orm::prelude::Expr::col(($col.entity_name(), $col))
             .cast_as(sea_orm::sea_query::Alias::new("text"))
             .eq($val)
     };
