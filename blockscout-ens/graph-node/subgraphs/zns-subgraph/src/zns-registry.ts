e6c0d51b054eb58ad8f928a3a9b0bcb2ce3281dc
sevenzing <41516657+sevenzing@users.noreply.github.com>
2024-05-29 17:53:29 +0200

    Fix subgraph error and add ZNS to prod config

--- blockscout-ens/graph-node/subgraphs/zns-subgraph/src/zns-registry.ts
+++ blockscout-ens/graph-node/subgraphs/zns-subgraph/src/zns-registry.ts
@@ -28,6 +28,8 @@ export function handleMintedDomain(event: MintedDomainEvent): void {
   );
   domain.resolvedAddress = event.params.owner.toHex();
   domain.registrant = event.params.owner.toHex();
+  domain.subdomainCount = 0;
+
   let ownerId = event.params.owner.toHex();
   let owner = Account.load(ownerId);
   if (!owner) {
@@ -79,6 +81,7 @@ export function handleTransfer(event: TransferEvent): void {
     domain.labelhash = Bytes.fromByteArray(
       crypto.keccak256(Bytes.fromUTF8(domain.labelName!))
     );
+    domain.subdomainCount = 0;
   }
   let transfer = new Transfer(event.transaction.hash.toHex());
   transfer.domain = domain.id;
