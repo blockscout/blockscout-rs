1c2a45152c514aa60e23005065de703c9337eece
Rim Rakhimov <rimrakhimov@gmail.com>
2024-06-19 23:10:35 +0400

    Fix test runs

--- smart-contract-verifier/smart-contract-verifier-server/tests/zksync_integration/main.rs
+++ smart-contract-verifier/smart-contract-verifier-server/tests/zksync_integration/main.rs
@@ -1,8 +1,16 @@
+use tempfile::TempDir;
+
 mod types;
 mod zksync_solidity;
 
-async fn start() -> url::Url {
-    let (settings, base) = {
+struct ServerMetadata {
+    base_url: url::Url,
+    _zk_compilers_tempdir: TempDir,
+    _evm_compilers_tempdir: TempDir,
+}
+
+async fn start() -> ServerMetadata {
+    let (settings, server) = {
         let mut settings = smart_contract_verifier_server::Settings::default();
         let (server_settings, base) =
             blockscout_service_launcher::test_server::get_test_server_settings();
@@ -12,20 +20,34 @@ async fn start() -> url::Url {
         settings.vyper.enabled = false;
         settings.sourcify.enabled = false;
 
+        let zk_compilers_tempdir =
+            tempfile::tempdir().expect("creation temporary directory for zk_compilers");
+        let evm_compilers_tempdir =
+            tempfile::tempdir().expect("creation temporary directory for evm_compilers");
+
         settings.zksync_solidity.enabled = true;
+        settings.zksync_solidity.zk_compilers_dir = zk_compilers_tempdir.path().to_path_buf();
+        settings.zksync_solidity.evm_compilers_dir = evm_compilers_tempdir.path().to_path_buf();
 
         settings.metrics.enabled = false;
         settings.tracing.enabled = false;
         settings.jaeger.enabled = false;
 
-        (settings, base)
+        (
+            settings,
+            ServerMetadata {
+                base_url: base,
+                _zk_compilers_tempdir: zk_compilers_tempdir,
+                _evm_compilers_tempdir: evm_compilers_tempdir,
+            },
+        )
     };
 
     blockscout_service_launcher::test_server::init_server(
         || smart_contract_verifier_server::run(settings),
-        &base,
+        &server.base_url,
     )
     .await;
 
-    base
+    server
 }
