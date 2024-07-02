101e97b3afefb148fa4c5e7ded3acb1b75e7143c
Alexander Filippov <aleksander.fill@gmail.com>
2024-07-02 13:07:45 +0300

    Fix gaps test

--- da-indexer/da-indexer-logic/src/eigenda/tests/batches.rs
+++ da-indexer/da-indexer-logic/src/eigenda/tests/batches.rs
@@ -10,12 +10,12 @@ async fn find_gaps() {
     insert_batches(&db.client(), heights).await;
 
     let gaps = batches::find_gaps(&db.client(), 53, 20009).await.unwrap();
-    assert!(gaps[0].start == 53 && gaps[0].end == 699);
-    assert!(gaps[1].start == 701 && gaps[1].end == 1199);
-    assert!(gaps[2].start == 1501 && gaps[2].end == 1699);
-    assert!(gaps[3].start == 1701 && gaps[3].end == 9399);
-    assert!(gaps[4].start == 9401 && gaps[4].end == 15599);
-    assert!(gaps[5].start == 15701 && gaps[5].end == 20009);
+    assert!(gaps[0].start == 53 && gaps[0].end == 700);
+    assert!(gaps[1].start == 700 && gaps[1].end == 1200);
+    assert!(gaps[2].start == 1500 && gaps[2].end == 1700);
+    assert!(gaps[3].start == 1700 && gaps[3].end == 9400);
+    assert!(gaps[4].start == 9400 && gaps[4].end == 15600);
+    assert!(gaps[5].start == 15700 && gaps[5].end == 20009);
 }
 
 #[tokio::test]
