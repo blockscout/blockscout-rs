8ce081a268b2ae584e57f431c9aa9d01e7fc4638
Rim Rakhimov <rimrakhimov@gmail.com>
2024-07-11 11:11:36 +0300

    feat: add DefaultRateLimiterMiddleware
    Allows not to import governor into client crates

--- libs/reqwest-rate-limiter/src/lib.rs
+++ libs/reqwest-rate-limiter/src/lib.rs
@@ -1,27 +1,48 @@
-use governor::{
-    clock::{Clock, ReasonablyRealtime},
-    middleware::RateLimitingMiddleware,
-    state::{NotKeyed, StateStore},
-    NotUntil, RateLimiter,
-};
+use std::num::NonZeroU32;
+use governor::{middleware, state, clock, NotUntil, RateLimiter};
 use reqwest::{Request, Response};
 use reqwest_middleware::{Middleware, Next};
 use std::sync::Arc;
 
+pub type DefaultRateLimiterMiddleware<
+    MW = middleware::NoOpMiddleware<<clock::DefaultClock as clock::Clock>::Instant>,
+> = RateLimiterMiddleware<state::direct::NotKeyed, state::InMemoryState, clock::DefaultClock, MW>;
+
+impl DefaultRateLimiterMiddleware {
+    pub fn per_second(max_burst: NonZeroU32) -> Self {
+        RateLimiterMiddleware::new(RateLimiter::direct(
+            governor::Quota::per_second(max_burst),
+        ))
+    }
+
+    pub fn per_minute(max_burst: NonZeroU32) -> Self {
+        RateLimiterMiddleware::new(RateLimiter::direct(
+            governor::Quota::per_minute(max_burst),
+        ))
+    }
+
+    pub fn per_hour(max_burst: NonZeroU32) -> Self {
+        RateLimiterMiddleware::new(RateLimiter::direct(
+            governor::Quota::per_hour(max_burst),
+        ))
+    }
+}
+
+#[derive(Clone)]
 pub struct RateLimiterMiddleware<K, S, C, MW>
 where
-    S: StateStore<Key = K>,
-    C: Clock,
-    MW: RateLimitingMiddleware<C::Instant>,
+    S: state::StateStore<Key = K>,
+    C: clock::Clock,
+    MW: middleware::RateLimitingMiddleware<C::Instant>,
 {
     rate_limiter: Arc<RateLimiter<K, S, C, MW>>,
 }
 
 impl<K, S, C, MW> RateLimiterMiddleware<K, S, C, MW>
 where
-    S: StateStore<Key = K>,
-    C: Clock,
-    MW: RateLimitingMiddleware<C::Instant>,
+    S: state::StateStore<Key = K>,
+    C: clock::Clock,
+    MW: middleware::RateLimitingMiddleware<C::Instant>,
 {
     pub fn new(rate_limiter: RateLimiter<K, S, C, MW>) -> Self {
         Self::new_arc(Arc::new(rate_limiter))
@@ -33,11 +54,11 @@ where
 }
 
 #[async_trait::async_trait]
-impl<S, C, MW, PO> Middleware for RateLimiterMiddleware<NotKeyed, S, C, MW>
+impl<S, C, MW, PO> Middleware for RateLimiterMiddleware<state::direct::NotKeyed, S, C, MW>
 where
-    S: StateStore<Key = NotKeyed> + Send + Sync + 'static,
-    C: Clock + ReasonablyRealtime + Send + Sync + 'static,
-    MW: RateLimitingMiddleware<
+    S: state::StateStore<Key = state::direct::NotKeyed> + Send + Sync + 'static,
+    C: clock::Clock + clock::ReasonablyRealtime + Send + Sync + 'static,
+    MW: middleware::RateLimitingMiddleware<
             C::Instant,
             NegativeOutcome = NotUntil<C::Instant>,
             PositiveOutcome = PO,
