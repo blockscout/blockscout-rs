984e81acfbfe290a234be25f130dff8676eca5c8
Kirill Ivanov <bragov4ik@hotmail.com>
2024-07-24 11:19:44 +0200

    remove parse+sum/cumsum fns since they repeat other functionality
    they repeat MapParseTo + sum/cumsum

--- stats/stats/src/data_processing.rs
+++ stats/stats/src/data_processing.rs
@@ -6,33 +6,9 @@ use crate::{
     UpdateError,
 };
 use std::{
-    fmt::Display,
     mem,
     ops::{AddAssign, SubAssign},
-    str::FromStr,
 };
-
-/// `prev_sum` - sum before this data segment
-pub fn parse_and_cumsum<T>(
-    mut data: Vec<DateValue<String>>,
-    parent_name: &str,
-    mut prev_sum: T,
-) -> Result<Vec<DateValue<String>>, UpdateError>
-where
-    T: AddAssign + FromStr + Default + Display,
-    T::Err: Display,
-{
-    for item in data.iter_mut() {
-        let value = item.value.parse::<T>().map_err(|e| {
-            UpdateError::Internal(format!(
-                "failed to parse values in chart '{parent_name}': {e}",
-            ))
-        })?;
-        prev_sum += value;
-        item.value = prev_sum.to_string();
-    }
-    Ok(data)
-}
 
 /// Allows missing dates in `data`.
 /// Assumes `data` is sorted.
@@ -81,45 +57,6 @@ where
     }
     Ok(data)
 }
-
-// todo: remove
-// pub fn parse_and_sum<T>(
-//     data: Vec<DateValueString>,
-//     chart_name: &str,
-//     parent_name: &str,
-// ) -> Result<Option<DateValueString>, UpdateError>
-// where
-//     T: Sum + FromStr + Default + Display,
-//     T::Err: Display,
-// {
-//     let max_date = match data.iter().max() {
-//         Some(max_date) => max_date.date,
-//         None => {
-//             tracing::warn!(
-//                 chart_name = chart_name,
-//                 parent_chart_name = parent_name,
-//                 "parent doesn't have any data after update"
-//             );
-//             return Ok(None);
-//         }
-//     };
-//     let total: T = data
-//         .into_iter()
-//         .map(|p| p.value.parse::<T>())
-//         .collect::<Result<Vec<_>, _>>()
-//         .map_err(|e| {
-//             UpdateError::Internal(format!(
-//                 "failed to parse values in chart '{parent_name}': {e}",
-//             ))
-//         })?
-//         .into_iter()
-//         .sum();
-//     let point = DateValueString {
-//         date: max_date,
-//         value: total.to_string(),
-//     };
-//     Ok(Some(point))
-// }
 
 pub fn sum<Resolution, Value>(
     data: &[TimespanValue<Resolution, Value>],
