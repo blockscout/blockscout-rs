d5af074dfbeaf9187425dc5c2fc7aa20c58e8d2f
sevenzing <41516657+sevenzing@users.noreply.github.com>
2024-07-26 12:16:05 +0200

    chore: fix pr comments

--- blockscout-ens/bens-logic/src/subgraph/ens/wildcard.rs
+++ blockscout-ens/bens-logic/src/subgraph/ens/wildcard.rs
@@ -101,18 +101,16 @@ async fn any_resolver(
     // try to find resolver in db
     let maybe_domain_with_resolver = any_resolver_in_db(db, name_options.clone()).await?;
     if let Some((resolver, found_domain)) = maybe_domain_with_resolver {
-        Ok(Some((resolver.resolver_address, Some(found_domain))))
-    } else if protocol.info.registry_contract.is_some() || protocol.info.network_id == 1 {
+        return Ok(Some((resolver.resolver_address, Some(found_domain))));
+    } else if protocol.info.registry_contract.is_some() {
         // try to find resolver on chain.
         // if custom registry is set, we can try to find resolver in registry
         if let Some(resolver_address) = any_resolver_rpc(name_options).await {
-            Ok(Some((resolver_address, None)))
-        } else {
-            Ok(None)
+            return Ok(Some((resolver_address, None)));
         }
-    } else {
-        Ok(None)
-    }
+    };
+
+    Ok(None)
 }
 
 async fn any_resolver_in_db(
@@ -128,22 +126,21 @@ async fn any_resolver_in_db(
             .context("searching parents in db")?
     };
 
-    let result = names
-        .iter()
-        .filter_map(|name| {
-            found_parent_domains
-                .iter()
-                .find(|d| d.name.as_ref() == Some(&name.inner.name))
-                .and_then(|domain| {
-                    let maybe_resolver = domain
-                        .resolver
-                        .as_ref()
-                        .and_then(|r| subgraph::ResolverInSubgraph::from_str(r).ok());
-                    maybe_resolver.map(|r| (r, domain.clone()))
-                })
-        })
-        .next();
-    Ok(result)
+    for name in names {
+        let maybe_domain = found_parent_domains
+            .iter()
+            .find(|d| d.name.as_ref() == Some(&name.inner.name));
+        if let Some(domain) = maybe_domain {
+            let maybe_resolver = domain
+                .resolver
+                .as_ref()
+                .and_then(|r| ResolverInSubgraph::from_str(r).ok());
+            if let Some(resolver) = maybe_resolver {
+                return Ok(Some((resolver, domain.clone())));
+            }
+        }
+    }
+    Ok(None)
 }
 
 async fn any_resolver_rpc(names: Vec<DomainNameOnProtocol<'_>>) -> Option<Address> {
