5618a34267c5edc0c85096b1bade8b527b4cb910
Kirill Ivanov <bragov4ik@hotmail.com>
2024-09-16 15:23:09 +0300

    restructure remote_db source

--- stats/stats/src/data_source/kinds/remote_db/query.rs
+++ stats/stats/src/data_source/kinds/remote_db/query.rs
@@ -1,82 +1,17 @@
-//! Remote database source.
-//!
-//! The main application - SQL queries from remote (=blockscout) database.
-//!
-//! ## Details
-//!
-//! This source does not have any persistency and is only an adapter for representing
-//! a remote DB as a `DataSource`.
-//!
-//! Since each [`RemoteQueryBehaviour::query_data`] performs (likely a heavy) database
-//! query, it is undesireable to have this source present in more than one place.
-//! Each (independent) appearance will likely result in requesting the same data
-//! multiple times
-//!
-//! In this case, persistent behaviour of
-//! [types in `local_db`](`crate::data_source::kinds::local_db`) is largely
-//! helpful.
-
 use std::{
-    future::Future,
     marker::{PhantomData, Send},
     ops::Range,
 };
 
-use blockscout_metrics_tools::AggregateTimer;
-use chrono::{DateTime, Utc};
-use sea_orm::{prelude::DateTimeUtc, DatabaseConnection, DbErr, FromQueryResult, Statement};
+use sea_orm::{prelude::DateTimeUtc, FromQueryResult, Statement};
 
 use crate::{
-    data_source::{
-        source::DataSource,
-        types::{BlockscoutMigrations, UpdateContext},
-    },
+    data_source::types::{BlockscoutMigrations, UpdateContext},
     types::TimespanValue,
     UpdateError,
 };
 
-/// See [module-level documentation](self)
-pub struct RemoteDatabaseSource<Q: RemoteQueryBehaviour>(PhantomData<Q>);
-
-pub trait RemoteQueryBehaviour {
-    type Output: Send;
-
-    /// Retrieve chart data from remote storage.
-    fn query_data(
-        cx: &UpdateContext<'_>,
-        range: Option<Range<DateTimeUtc>>,
-    ) -> impl Future<Output = Result<Self::Output, UpdateError>> + Send;
-}
-
-impl<Q: RemoteQueryBehaviour> DataSource for RemoteDatabaseSource<Q> {
-    type MainDependencies = ();
-    type ResolutionDependencies = ();
-    type Output = Q::Output;
-    // No local state => no race conditions expected
-    fn mutex_id() -> Option<String> {
-        None
-    }
-
-    async fn init_itself(
-        _db: &DatabaseConnection,
-        _init_time: &DateTime<Utc>,
-    ) -> Result<(), DbErr> {
-        Ok(())
-    }
-
-    async fn query_data(
-        cx: &UpdateContext<'_>,
-        range: Option<Range<DateTimeUtc>>,
-        remote_fetch_timer: &mut AggregateTimer,
-    ) -> Result<<Self as DataSource>::Output, UpdateError> {
-        let _interval = remote_fetch_timer.start_interval();
-        Q::query_data(cx, range).await
-    }
-
-    async fn update_itself(_cx: &UpdateContext<'_>) -> Result<(), UpdateError> {
-        Ok(())
-    }
-}
+use super::RemoteQueryBehaviour;
 
 pub trait StatementFromRange {
     /// `completed_migrations` can be used for selecting more optimal query
