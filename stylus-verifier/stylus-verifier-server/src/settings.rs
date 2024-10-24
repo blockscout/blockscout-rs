67050eece174fa6d8e2b494d4ec40312772a25c8
Rim Rakhimov <rimrakhimov@gmail.com>
2024-10-24 12:17:05 +0400

    feat(stylus-verifier): make use of docker api (#1074)
    * feat(stylus-verifier): make use of docker api
    
    * feat(stylus-verifier): add docker api url argument into settings
    
    * feat(stylus-verifier): add support unix-based docker api addresses
    
    * fix(stylus-verifier): upload project directory into github container explicitly
    
    * refactor(stylus-verifier): add trace logging on image build; rename start_container to run_container

--- stylus-verifier/stylus-verifier-server/src/settings.rs
+++ stylus-verifier/stylus-verifier-server/src/settings.rs
@@ -3,6 +3,7 @@ use blockscout_service_launcher::{
     tracing::{JaegerSettings, TracingSettings},
 };
 use serde::Deserialize;
+use url::Url;
 
 #[derive(Debug, Default, Deserialize, Clone, PartialEq, Eq)]
 #[serde(default, deny_unknown_fields)]
@@ -11,6 +12,21 @@ pub struct Settings {
     pub metrics: MetricsSettings,
     pub tracing: TracingSettings,
     pub jaeger: JaegerSettings,
+    pub docker_api: DockerApiSettings,
+}
+
+#[derive(Debug, Deserialize, Clone, PartialEq, Eq)]
+#[serde(default, deny_unknown_fields)]
+pub struct DockerApiSettings {
+    pub addr: Url,
+}
+
+impl Default for DockerApiSettings {
+    fn default() -> Self {
+        Self {
+            addr: Url::parse("unix:///var/run/docker.sock").expect("default docker api url"),
+        }
+    }
 }
 
 impl ConfigSettings for Settings {
