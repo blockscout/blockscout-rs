8cdc0c0c5eeef3081efcc4af59622a02230134c2
Rim Rakhimov <rimrakhimov@gmail.com>
2024-10-28 18:35:38 +0400

    feat(eth-bytecode-db): update verifier alliance schema to v1; regenerate entities

--- eth-bytecode-db/eth-bytecode-db/verifier-alliance-entity/src/sources.rs
+++ eth-bytecode-db/eth-bytecode-db/verifier-alliance-entity/src/sources.rs
@@ -3,25 +3,33 @@
 use sea_orm::entity::prelude::*;
 
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq)]
-#[sea_orm(table_name = "code")]
+#[sea_orm(table_name = "sources")]
 pub struct Model {
     #[sea_orm(
         primary_key,
         auto_increment = false,
         column_type = "VarBinary(StringLen::None)"
     )]
-    pub code_hash: Vec<u8>,
+    pub source_hash: Vec<u8>,
+    #[sea_orm(column_type = "VarBinary(StringLen::None)")]
+    pub source_hash_keccak: Vec<u8>,
+    pub content: String,
     pub created_at: DateTimeWithTimeZone,
     pub updated_at: DateTimeWithTimeZone,
     pub created_by: String,
     pub updated_by: String,
-    #[sea_orm(column_type = "VarBinary(StringLen::None)")]
-    pub code_hash_keccak: Vec<u8>,
-    #[sea_orm(column_type = "VarBinary(StringLen::None)", nullable)]
-    pub code: Option<Vec<u8>>,
 }
 
 #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-pub enum Relation {}
+pub enum Relation {
+    #[sea_orm(has_many = "super::compiled_contracts_sources::Entity")]
+    CompiledContractsSources,
+}
+
+impl Related<super::compiled_contracts_sources::Entity> for Entity {
+    fn to() -> RelationDef {
+        Relation::CompiledContractsSources.def()
+    }
+}
 
 impl ActiveModelBehavior for ActiveModel {}
