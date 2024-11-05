9b4c3785bf137b6ad4aeb5acb955f252f9d7d876
Leonid Tyurin <lenyatyurin162586@gmail.com>
2024-11-05 18:33:24 +0700

    chore: update type

--- multichain-aggregator/multichain-aggregator-logic/src/repository/api_keys.rs
+++ multichain-aggregator/multichain-aggregator-logic/src/repository/api_keys.rs
@@ -1,11 +1,11 @@
-use crate::types::api_keys::ApiKey;
+use crate::types::{api_keys::ApiKey, ChainId};
 use entity::api_keys::{Column, Entity};
 use sea_orm::{prelude::Uuid, ColumnTrait, DatabaseConnection, DbErr, EntityTrait, QueryFilter};
 
 pub async fn find_by_key_and_chain_id(
     db: &DatabaseConnection,
     key: Uuid,
-    chain_id: i64,
+    chain_id: ChainId,
 ) -> Result<Option<ApiKey>, DbErr> {
     let api_key = Entity::find()
         .filter(Column::Key.eq(key))
