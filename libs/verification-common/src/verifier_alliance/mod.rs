a47d2c785fde2e8b16629048cf20720e0c9b30a7
Rim Rakhimov <rimrakhimov@gmail.com>
2024-11-05 15:33:58 +0400

    (verification-common): update artifacts to serialize absent fields as nulls

--- libs/verification-common/src/verifier_alliance/mod.rs
+++ libs/verification-common/src/verifier_alliance/mod.rs
@@ -6,7 +6,7 @@ mod verification_match;
 mod verification_match_transformations;
 mod verification_match_values;
 
-pub use compilation_artifacts::{CompilationArtifacts, ToCompilationArtifacts};
+pub use compilation_artifacts::{CompilationArtifacts, SourceId, ToCompilationArtifacts};
 pub use creation_code_artifacts::{CreationCodeArtifacts, ToCreationCodeArtifacts};
 pub use runtime_code_artifacts::{RuntimeCodeArtifacts, ToRuntimeCodeArtifacts};
 pub use verification_match::{Match, MatchBuilder, MatchTransformation, MatchType, MatchValues};
