59c784f80c7140149bde185e806fcca5b8f64bc7
Kirill Ivanov <bragov4ik@hotmail.com>
2024-11-21 16:09:45 +0900

    estimation fallback

--- stats/stats/src/data_source/kinds/local_db/parameters/query.rs
+++ stats/stats/src/data_source/kinds/local_db/parameters/query.rs
@@ -1,6 +1,6 @@
 use std::{fmt::Debug, marker::PhantomData, ops::Range};
 
-use sea_orm::prelude::DateTimeUtc;
+use sea_orm::{prelude::DateTimeUtc, DatabaseConnection};
 
 use crate::{
     charts::db_interaction::read::get_counter_data,
@@ -90,9 +90,21 @@ impl<C: ChartProperties> QueryBehaviour for DefaultQueryLast<C> {
     }
 }
 
-pub struct QueryLastWithEstimationFallback<C: ChartProperties>(PhantomData<C>);
+#[trait_variant::make(Send)]
+pub trait ValueEstimation {
+    async fn estimate(blockscout: &DatabaseConnection) -> Result<DateValue<String>, UpdateError>;
+}
 
-impl<C: ChartProperties> QueryBehaviour for QueryLastWithEstimationFallback<C> {
+pub struct QueryLastWithEstimationFallback<E, C>(PhantomData<(E, C)>)
+where
+    C: ChartProperties,
+    E: ValueEstimation;
+
+impl<E, C> QueryBehaviour for QueryLastWithEstimationFallback<E, C>
+where
+    C: ChartProperties,
+    E: ValueEstimation,
+{
     type Output = DateValue<String>;
 
     async fn query_data(
@@ -106,10 +118,86 @@ impl<C: ChartProperties> QueryBehaviour for QueryLastWithEstimationFallback<C> {
             C::missing_date_policy(),
         )
         .await?
-        .ok_or(UpdateError::Internal(format!(
-            "no data for counter '{}' was found",
-            C::name()
-        )))?;
+        .unwrap_or(E::estimate(cx.blockscout).await?);
         Ok(value)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use chrono::NaiveDate;
+    use entity::sea_orm_active_enums::ChartType;
+    use pretty_assertions::assert_eq;
+    use sea_orm::DatabaseConnection;
+
+    use super::*;
+
+    use crate::{
+        data_source::{types::BlockscoutMigrations, UpdateContext, UpdateParameters},
+        tests::init_db::init_db_all,
+        types::timespans::DateValue,
+        MissingDatePolicy, Named, UpdateError,
+    };
+
+    #[tokio::test]
+    #[ignore = "needs database to run"]
+    async fn fallback_query_works() {
+        let _ = tracing_subscriber::fmt::try_init();
+        let (db, blockscout) = init_db_all("fallback_query_works").await;
+        let current_time = chrono::DateTime::from_str("2023-03-01T12:00:00Z").unwrap();
+
+        let parameters = UpdateParameters {
+            db: &db,
+            blockscout: &blockscout,
+            blockscout_applied_migrations: BlockscoutMigrations::latest(),
+            update_time_override: Some(current_time),
+            force_full: true,
+        };
+        let cx = UpdateContext::from_params_now_or_override(parameters.clone());
+
+        struct TestFallback;
+
+        fn expected_estimate() -> DateValue<String> {
+            DateValue {
+                timespan: chrono::NaiveDate::MAX,
+                value: "estimate".to_string(),
+            }
+        }
+
+        impl ValueEstimation for TestFallback {
+            async fn estimate(
+                _blockscout: &DatabaseConnection,
+            ) -> Result<DateValue<String>, UpdateError> {
+                Ok(expected_estimate())
+            }
+        }
+
+        pub struct InvalidProperties;
+        impl Named for InvalidProperties {
+            fn name() -> String {
+                "totalBlocks".into()
+            }
+        }
+        impl ChartProperties for InvalidProperties {
+            type Resolution = NaiveDate;
+
+            fn chart_type() -> ChartType {
+                ChartType::Counter
+            }
+            fn missing_date_policy() -> MissingDatePolicy {
+                MissingDatePolicy::FillPrevious
+            }
+        }
+
+        assert_eq!(
+            expected_estimate(),
+            QueryLastWithEstimationFallback::<TestFallback, InvalidProperties>::query_data(
+                &cx, None
+            )
+            .await
+            .unwrap()
+        );
     }
 }
