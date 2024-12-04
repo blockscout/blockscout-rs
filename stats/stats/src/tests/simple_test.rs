fae1d75d0151218ef55e059a231fc18bf72b652d
Kirill Ivanov <bragov4ik@hotmail.com>
2024-12-04 23:47:36 +0900

    don't use raw db read for chart tests

--- stats/stats/src/tests/simple_test.rs
+++ stats/stats/src/tests/simple_test.rs
@@ -7,17 +7,16 @@ use crate::{
         source::DataSource,
         types::{BlockscoutMigrations, UpdateContext, UpdateParameters},
     },
-    get_line_chart_data,
     query_dispatch::QuerySerialized,
     range::UniversalRange,
     types::{timespans::DateValue, Timespan},
     utils::MarkedDbConnection,
-    ChartProperties, MissingDatePolicy,
+    ChartProperties,
 };
 use blockscout_service_launcher::test_database::TestDbGuard;
 use chrono::{DateTime, NaiveDateTime, Utc};
 use pretty_assertions::assert_eq;
-use sea_orm::DatabaseConnection;
+use stats_proto::blockscout::stats::v1::Point;
 use std::{fmt::Debug, str::FromStr};
 
 pub fn map_str_tuple_to_owned(l: Vec<(&str, &str)>) -> Vec<(String, String)> {
@@ -39,7 +38,7 @@ pub async fn simple_test_chart<C>(
     expected: Vec<(&str, &str)>,
 ) -> (TestDbGuard, TestDbGuard)
 where
-    C: DataSource + ChartProperties,
+    C: DataSource + ChartProperties + QuerySerialized<Output = Vec<Point>>,
     C::Resolution: Ord + Clone + Debug,
 {
     simple_test_chart_inner::<C>(test_name, expected, BlockscoutMigrations::latest()).await
@@ -56,7 +55,7 @@ pub async fn simple_test_chart_with_migration_variants<C>(
     test_name_base: &str,
     expected: Vec<(&str, &str)>,
 ) where
-    C: DataSource + ChartProperties,
+    C: DataSource + ChartProperties + QuerySerialized<Output = Vec<Point>>,
     C::Resolution: Ord + Clone + Debug,
 {
     for (i, migrations) in MIGRATIONS_VARIANTS.into_iter().enumerate() {
@@ -65,18 +64,21 @@ pub async fn simple_test_chart_with_migration_variants<C>(
     }
 }
 
+fn chart_output_to_expected(output: Vec<Point>) -> Vec<(String, String)> {
+    output.into_iter().map(|p| (p.date, p.value)).collect()
+}
+
 async fn simple_test_chart_inner<C>(
     test_name: &str,
     expected: Vec<(&str, &str)>,
     migrations: BlockscoutMigrations,
 ) -> (TestDbGuard, TestDbGuard)
 where
-    C: DataSource + ChartProperties,
+    C: DataSource + ChartProperties + QuerySerialized<Output = Vec<Point>>,
     C::Resolution: Ord + Clone + Debug,
 {
     let (current_time, db, blockscout) = prepare_chart_test::<C>(test_name, None).await;
     let expected = map_str_tuple_to_owned(expected);
-    let approximate_trailing_points = C::approximate_trailing_points();
     let current_date = current_time.date_naive();
     fill_mock_blockscout_data(&blockscout, current_date).await;
 
@@ -90,15 +92,11 @@ where
     let cx = UpdateContext::from_params_now_or_override(parameters.clone());
     C::update_recursively(&cx).await.unwrap();
     assert_eq!(
-        &get_chart::<C>(
-            &db,
-            None,
-            None,
-            C::missing_date_policy(),
-            false,
-            approximate_trailing_points,
-        )
-        .await,
+        &chart_output_to_expected(
+            C::query_data_static(&cx, UniversalRange::full(), None, false)
+                .await
+                .unwrap()
+        ),
         &expected
     );
 
@@ -106,15 +104,11 @@ where
     let cx = UpdateContext::from_params_now_or_override(parameters);
     C::update_recursively(&cx).await.unwrap();
     assert_eq!(
-        &get_chart::<C>(
-            &db,
-            None,
-            None,
-            C::missing_date_policy(),
-            false,
-            approximate_trailing_points,
-        )
-        .await,
+        &chart_output_to_expected(
+            C::query_data_static(&cx, UniversalRange::full(), None, false)
+                .await
+                .unwrap()
+        ),
         &expected
     );
     (db, blockscout)
@@ -129,7 +123,7 @@ pub async fn dirty_force_update_and_check<C>(
     expected: Vec<(&str, &str)>,
     update_time_override: Option<DateTime<Utc>>,
 ) where
-    C: DataSource + ChartProperties,
+    C: DataSource + ChartProperties + QuerySerialized<Output = Vec<Point>>,
     C::Resolution: Ord + Clone + Debug,
 {
     let _ = tracing_subscriber::fmt::try_init();
@@ -137,7 +131,6 @@ pub async fn dirty_force_update_and_check<C>(
     // some later time so that the update is not skipped
     let current_time =
         update_time_override.unwrap_or(DateTime::from_str("2023-03-01T12:00:01Z").unwrap());
-    let approximate_trailing_points = C::approximate_trailing_points();
 
     let parameters = UpdateParameters {
         db: &MarkedDbConnection::from_test_db(db).unwrap(),
@@ -149,15 +142,11 @@ pub async fn dirty_force_update_and_check<C>(
     let cx = UpdateContext::from_params_now_or_override(parameters.clone());
     C::update_recursively(&cx).await.unwrap();
     assert_eq!(
-        &get_chart::<C>(
-            db,
-            None,
-            None,
-            C::missing_date_policy(),
-            false,
-            approximate_trailing_points,
-        )
-        .await,
+        &chart_output_to_expected(
+            C::query_data_static(&cx, UniversalRange::full(), None, false)
+                .await
+                .unwrap()
+        ),
         &expected
     );
 }
@@ -176,7 +165,7 @@ pub async fn ranged_test_chart<C>(
     to: C::Resolution,
     update_time: Option<NaiveDateTime>,
 ) where
-    C: DataSource + ChartProperties,
+    C: DataSource + ChartProperties + QuerySerialized<Output = Vec<Point>>,
     C::Resolution: Ord + Clone + Debug,
 {
     ranged_test_chart_inner::<C>(
@@ -204,7 +193,7 @@ pub async fn ranged_test_chart_with_migration_variants<C>(
     to: C::Resolution,
     update_time: Option<NaiveDateTime>,
 ) where
-    C: DataSource + ChartProperties,
+    C: DataSource + ChartProperties + QuerySerialized<Output = Vec<Point>>,
     C::Resolution: Ord + Clone + Debug,
 {
     for (i, migrations) in MIGRATIONS_VARIANTS.into_iter().enumerate() {
@@ -229,7 +218,7 @@ async fn ranged_test_chart_inner<C>(
     update_time: Option<NaiveDateTime>,
     migrations: BlockscoutMigrations,
 ) where
-    C: DataSource + ChartProperties,
+    C: DataSource + ChartProperties + QuerySerialized<Output = Vec<Point>>,
     C::Resolution: Ord + Clone + Debug,
 {
     let _ = tracing_subscriber::fmt::try_init();
@@ -238,12 +227,11 @@ async fn ranged_test_chart_inner<C>(
     let max_time = DateTime::<Utc>::from_str("2023-03-01T12:00:00Z").unwrap();
     let current_time = update_time.map(|t| t.and_utc()).unwrap_or(max_time);
     let max_date = max_time.date_naive();
+    let range = { from.into_time_range().start..to.into_time_range().end };
     C::init_recursively(db.connection.as_ref(), &current_time)
         .await
         .unwrap();
     fill_mock_blockscout_data(blockscout.connection.as_ref(), max_date).await;
-    let policy = C::missing_date_policy();
-    let approximate_trailing_points = C::approximate_trailing_points();
 
     let mut parameters = UpdateParameters {
         db: &db,
@@ -255,15 +243,11 @@ async fn ranged_test_chart_inner<C>(
     let cx = UpdateContext::from_params_now_or_override(parameters.clone());
     C::update_recursively(&cx).await.unwrap();
     assert_eq!(
-        &get_chart::<C>(
-            db.connection.as_ref(),
-            Some(from.clone()),
-            Some(to.clone()),
-            policy,
-            false,
-            approximate_trailing_points,
-        )
-        .await,
+        &chart_output_to_expected(
+            C::query_data_static(&cx, range.clone().into(), None, false)
+                .await
+                .unwrap()
+        ),
         &expected
     );
 
@@ -271,46 +255,13 @@ async fn ranged_test_chart_inner<C>(
     let cx = UpdateContext::from_params_now_or_override(parameters);
     C::update_recursively(&cx).await.unwrap();
     assert_eq!(
-        &get_chart::<C>(
-            db.connection.as_ref(),
-            Some(from),
-            Some(to),
-            policy,
-            false,
-            approximate_trailing_points,
-        )
-        .await,
+        &chart_output_to_expected(
+            C::query_data_static(&cx, range.into(), None, false)
+                .await
+                .unwrap()
+        ),
         &expected
     );
-}
-
-async fn get_chart<C>(
-    db: &DatabaseConnection,
-    from: Option<C::Resolution>,
-    to: Option<C::Resolution>,
-    policy: MissingDatePolicy,
-    fill_missing_dates: bool,
-    approximate_trailing_points: u64,
-) -> Vec<(String, String)>
-where
-    C: DataSource + ChartProperties,
-    C::Resolution: Ord + Clone + Debug,
-{
-    let data = get_line_chart_data::<C::Resolution>(
-        db,
-        &C::name(),
-        from,
-        to,
-        None,
-        policy,
-        fill_missing_dates,
-        approximate_trailing_points,
-    )
-    .await
-    .unwrap();
-    data.into_iter()
-        .map(|p| (p.timespan.into_date().to_string(), p.value))
-        .collect()
 }
 
 /// `test_name` must be unique to avoid db clashes
@@ -395,9 +346,7 @@ pub async fn prepare_chart_test<C: DataSource + ChartProperties>(
 pub async fn get_counter<C: QuerySerialized<Output = DateValue<String>>>(
     cx: &UpdateContext<'_>,
 ) -> DateValue<String> {
-    let counter_object = <C as QuerySerialized>::new_for_dynamic_dispatch();
-    counter_object
-        .query_data(cx, UniversalRange::full(), None, false)
+    C::query_data_static(cx, UniversalRange::full(), None, false)
         .await
         .unwrap()
 }
