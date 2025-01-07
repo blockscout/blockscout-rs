68cbe8752c2db0723c09851aff9a408481fab4ae
Kirill Ivanov <bragov4ik@hotmail.com>
2025-01-07 11:46:59 +0500

    clippy

--- stats/stats-server/src/blockscout_waiter.rs
+++ stats/stats-server/src/blockscout_waiter.rs
@@ -235,7 +235,7 @@ mod tests {
                 panic!("aggregator terminated: {:?}", res)
             }
             listener = wait_for_listener_timeout => {
-                return listener.map(|a| a.map_err(|e| e.into()))
+                listener.map(|a| a.map_err(|e| e.into()))
             }
         }
     }
