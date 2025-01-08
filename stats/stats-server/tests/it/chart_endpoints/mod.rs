47a6ff7110e876c8ba0b9f8ffd080187a1fdff80
Kirill Ivanov <bragov4ik@hotmail.com>
2025-01-08 12:37:48 +0500

    extract common test settings logic

--- stats/stats-server/tests/it/chart_endpoints/mod.rs
+++ stats/stats-server/tests/it/chart_endpoints/mod.rs
@@ -5,12 +5,9 @@
 //! procedure (i.e. to not initialize the read-only
 //! server each time).
 
-use std::{path::PathBuf, str::FromStr};
+use std::str::FromStr;
 
-use blockscout_service_launcher::{
-    launcher::ConfigSettings,
-    test_server::{get_test_server_settings, init_server},
-};
+use blockscout_service_launcher::test_server::init_server;
 use chrono::NaiveDate;
 use contracts_page::test_contracts_page_ok;
 use counters::test_counters_ok;
@@ -21,9 +18,11 @@ use stats::tests::{
     init_db::init_db_all,
     mock_blockscout::{default_mock_blockscout_api, fill_mock_blockscout_data},
 };
-use stats_server::{stats, Settings};
+use stats_server::stats;
 use tokio::task::JoinSet;
 use transactions_page::test_transactions_page_ok;
+
+use crate::common::get_test_stats_settings;
 
 mod contracts_page;
 mod counters;
@@ -37,17 +36,8 @@ async fn test_chart_endpoints_ok() {
     let (stats_db, blockscout_db) = init_db_all("test_chart_endpoints_ok").await;
     let blockscout_api = default_mock_blockscout_api().await;
     fill_mock_blockscout_data(&blockscout_db, NaiveDate::from_str("2023-03-01").unwrap()).await;
-
     std::env::set_var("STATS__CONFIG", "./tests/config/test.toml");
-    let mut settings = Settings::build().expect("Failed to build settings");
-    let (server_settings, base) = get_test_server_settings();
-    settings.server = server_settings;
-    settings.charts_config = PathBuf::from_str("../config/charts.json").unwrap();
-    settings.layout_config = PathBuf::from_str("../config/layout.json").unwrap();
-    settings.update_groups_config = PathBuf::from_str("../config/update_groups.json").unwrap();
-    settings.db_url = stats_db.db_url();
-    settings.blockscout_db_url = blockscout_db.db_url();
-    settings.blockscout_api_url = Some(url::Url::from_str(&blockscout_api.uri()).unwrap());
+    let (settings, base) = get_test_stats_settings(&stats_db, &blockscout_db, &blockscout_api);
 
     init_server(|| stats(settings), &base).await;
 
--- stats/stats-server/tests/it/common.rs
+++ stats/stats-server/tests/it/common.rs
@@ -1,6 +1,12 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, path::PathBuf, str::FromStr};
 
+use blockscout_service_launcher::{
+    launcher::ConfigSettings, test_database::TestDbGuard, test_server::get_test_server_settings,
+};
 use reqwest::{RequestBuilder, Response};
+use stats_server::Settings;
+use url::Url;
+use wiremock::MockServer;
 
 pub async fn send_arbitrary_request(request: RequestBuilder) -> Response {
     let response = request
@@ -25,6 +31,23 @@ pub async fn enabled_resolutions(
         .flat_map(|sec| sec.charts.clone())
         .map(|l| (l.id, l.resolutions))
         .collect()
+}
+
+pub fn get_test_stats_settings(
+    stats_db: &TestDbGuard,
+    blockscout_db: &TestDbGuard,
+    blockscout_api: &MockServer,
+) -> (Settings, Url) {
+    let mut settings = Settings::build().expect("Failed to build settings");
+    let (server_settings, base) = get_test_server_settings();
+    settings.server = server_settings;
+    settings.charts_config = PathBuf::from_str("../config/charts.json").unwrap();
+    settings.layout_config = PathBuf::from_str("../config/layout.json").unwrap();
+    settings.update_groups_config = PathBuf::from_str("../config/update_groups.json").unwrap();
+    settings.db_url = stats_db.db_url();
+    settings.blockscout_db_url = blockscout_db.db_url();
+    settings.blockscout_api_url = Some(url::Url::from_str(&blockscout_api.uri()).unwrap());
+    (settings, base)
 }
 
 #[macro_export]
