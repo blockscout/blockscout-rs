5e2369171c05e89464dc7ef0d6ace5167dd130f8
Kirill Ivanov <bragov4ik@hotmail.com>
2025-01-31 20:24:37 +0500

    save ~700 ms by reusing mock blockscout db

--- stats/stats-server/tests/it/chart_endpoints/mod.rs
+++ stats/stats-server/tests/it/chart_endpoints/mod.rs
@@ -4,10 +4,12 @@
 //! to simplify and optimize the server initialization
 //! procedure (i.e. to not initialize the read-only
 //! server each time).
+//!
+//! Note: all tests should be read only
 
-use std::str::FromStr;
+use std::{str::FromStr, time::Instant};
 
-use blockscout_service_launcher::test_server::init_server;
+use blockscout_service_launcher::{test_database::TestDbGuard, test_server::init_server};
 use chrono::NaiveDate;
 use contracts_page::test_contracts_page_ok;
 use counters::test_counters_ok;
@@ -15,7 +17,7 @@ use futures::FutureExt;
 use lines::test_lines_ok;
 use main_page::test_main_page_ok;
 use stats::tests::{
-    init_db::init_db_all,
+    init_db::{init_db, init_db_blockscout},
     mock_blockscout::{
         default_mock_blockscout_api, fill_mock_blockscout_data, mock_blockscout_api,
     },
@@ -35,11 +37,25 @@ mod transactions_page;
 
 #[tokio::test]
 #[ignore = "needs database"]
-async fn test_chart_endpoints_ok() {
-    let test_name = "test_chart_endpoints_ok";
-    let (stats_db, blockscout_db) = init_db_all("test_chart_endpoints_ok").await;
-    let blockscout_api = default_mock_blockscout_api().await;
+async fn tests_with_mock_blockscout() {
+    let test_name = "tests_with_mock_blockscout";
+    let blockscout_db = init_db_blockscout(test_name).await;
     fill_mock_blockscout_data(&blockscout_db, NaiveDate::from_str("2023-03-01").unwrap()).await;
+
+    let tests: JoinSet<_> = [
+        test_chart_endpoints_ok(blockscout_db.clone()).boxed(),
+        test_chart_endpoints_work_with_disabled_arbitrum(blockscout_db.clone()).boxed(),
+        test_chart_endpoints_work_with_not_indexed_blockscout(blockscout_db).boxed(),
+    ]
+    .into_iter()
+    .collect();
+    run_consolidated_tests(tests, test_name).await;
+}
+
+async fn test_chart_endpoints_ok(blockscout_db: TestDbGuard) {
+    let test_name = "test_chart_endpoints_ok";
+    let stats_db = init_db(test_name).await;
+    let blockscout_api = default_mock_blockscout_api().await;
     std::env::set_var("STATS__CONFIG", "./tests/config/test.toml");
     let (settings, base) = get_test_stats_settings(&stats_db, &blockscout_db, &blockscout_api);
 
@@ -60,11 +76,9 @@ async fn test_chart_endpoints_ok() {
     run_consolidated_tests(tests, test_name).await;
 }
 
-#[tokio::test]
-#[ignore = "needs database"]
-async fn test_chart_endpoints_work_with_not_indexed_blockscout() {
+async fn test_chart_endpoints_work_with_not_indexed_blockscout(blockscout_db: TestDbGuard) {
     let test_name = "test_chart_endpoints_work_with_not_indexed_blockscout";
-    let (stats_db, blockscout_db) = init_db_all(test_name).await;
+    let stats_db = init_db(test_name).await;
     let blockscout_api = mock_blockscout_api(ResponseTemplate::new(200).set_body_string(
         r#"{
             "finished_indexing": false,
@@ -74,7 +88,6 @@ async fn test_chart_endpoints_work_with_not_indexed_blockscout() {
         }"#,
     ))
     .await;
-    fill_mock_blockscout_data(&blockscout_db, NaiveDate::from_str("2023-03-01").unwrap()).await;
     std::env::set_var("STATS__CONFIG", "./tests/config/test.toml");
     let (settings, base) = get_test_stats_settings(&stats_db, &blockscout_db, &blockscout_api);
 
@@ -93,13 +106,10 @@ async fn test_chart_endpoints_work_with_not_indexed_blockscout() {
     run_consolidated_tests(tests, test_name).await;
 }
 
-#[tokio::test]
-#[ignore = "needs database"]
-async fn test_chart_endpoints_work_with_disabled_arbitrum() {
+async fn test_chart_endpoints_work_with_disabled_arbitrum(blockscout_db: TestDbGuard) {
     let test_name = "test_chart_endpoints_work_with_disabled_arbitrum";
-    let (stats_db, blockscout_db) = init_db_all(test_name).await;
+    let stats_db = init_db(test_name).await;
     let blockscout_api = default_mock_blockscout_api().await;
-    fill_mock_blockscout_data(&blockscout_db, NaiveDate::from_str("2023-03-01").unwrap()).await;
     std::env::set_var("STATS__CONFIG", "./tests/config/test.toml");
     let (mut settings, base) = get_test_stats_settings(&stats_db, &blockscout_db, &blockscout_api);
     settings.enable_all_arbitrum = false;
