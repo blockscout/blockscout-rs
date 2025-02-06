04d303372cda131f978663c016ea0571f498a867
Rim Rakhimov <rimrakhimov@gmail.com>
2025-02-06 13:31:07 +0400

    chore(verifier): bump actix-prost dependencies (#1233)
    * bump actix-prost dependencies to v0.1.0 
    
    * bump tonic dependencies to v0.12
    
    * bump prost dependencies to v0.13 
    
    * bump blockscout-service-launcher to v0.17.0

--- smart-contract-verifier/smart-contract-verifier-server/src/services/vyper_verifier.rs
+++ smart-contract-verifier/smart-contract-verifier-server/src/services/vyper_verifier.rs
@@ -82,7 +82,7 @@ impl VyperVerifier for VyperVerifierService {
 
         tracing::debug!(
             bytecode = request.bytecode,
-            bytecode_type = BytecodeType::from_i32(request.bytecode_type)
+            bytecode_type = BytecodeType::try_from(request.bytecode_type)
                 .unwrap()
                 .as_str_name(),
             compiler_version = request.compiler_version,
@@ -153,7 +153,7 @@ impl VyperVerifier for VyperVerifierService {
 
         tracing::debug!(
             bytecode = request.bytecode,
-            bytecode_type = BytecodeType::from_i32(request.bytecode_type)
+            bytecode_type = BytecodeType::try_from(request.bytecode_type)
                 .unwrap()
                 .as_str_name(),
             compiler_version = request.compiler_version,
