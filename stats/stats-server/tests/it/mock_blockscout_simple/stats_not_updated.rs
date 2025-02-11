14140ee11c041a9e02914fdb7817632e907b07fb
Kirill Ivanov <bragov4ik@hotmail.com>
2025-02-11 17:42:18 +0300

    move infinite sleep closer to its reason + add user ops endpoint to tests

--- stats/stats-server/tests/it/mock_blockscout_simple/stats_not_updated.rs
+++ stats/stats-server/tests/it/mock_blockscout_simple/stats_not_updated.rs
@@ -8,15 +8,17 @@ use crate::common::get_test_stats_settings;
 pub async fn run_tests_with_charts_not_updated(blockscout_db: TestDbGuard) {
     let test_name = "run_tests_with_charts_not_updated";
     let stats_db = init_db(test_name).await;
-    let blockscout_api = mock_blockscout_api(ResponseTemplate::new(200).set_body_string(
-        r#"{
-            "finished_indexing": false,
-            "finished_indexing_blocks": false,
-            "indexed_blocks_ratio": "0.00",
-            "indexed_internal_transactions_ratio": null
-        }"#,
-    ))
-    .await;
+    let blockscout_api = mock_blockscout_api(
+        ResponseTemplate::new(200).set_body_string(
+            r#"{
+                "finished_indexing": false,
+                "finished_indexing_blocks": false,
+                "indexed_blocks_ratio": "0.00",
+                "indexed_internal_transactions_ratio": "0.00"
+            }"#,
+        ),
+        Some(ResponseTemplate::new(200).set_body_string(user_ops_status_response_json(false))),
+    );
     std::env::set_var("STATS__CONFIG", "./tests/config/test.toml");
     let (mut settings, base) = get_test_stats_settings(&stats_db, &blockscout_db, &blockscout_api);
     // will not update at all
