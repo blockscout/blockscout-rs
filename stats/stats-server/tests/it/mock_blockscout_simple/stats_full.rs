93f39493b5c357c89096271cdb01b34950e74c85
Kirill Ivanov <bragov4ik@hotmail.com>
2025-02-12 19:01:41 +0300

    notify on finished init updates (doesn't work when not fully indexed)

--- stats/stats-server/tests/it/mock_blockscout_simple/stats_full.rs
+++ stats/stats-server/tests/it/mock_blockscout_simple/stats_full.rs
@@ -2,13 +2,13 @@
 //! - blockscout is fully indexed
 //! - stats server is fully enabled & initialized
 
-use std::time::Duration;
+use std::sync::Arc;
 
 use blockscout_service_launcher::test_server::init_server;
 use futures::FutureExt;
 use stats::tests::{init_db::init_db, mock_blockscout::default_mock_blockscout_api};
 use stats_server::stats;
-use tokio::{task::JoinSet, time::sleep};
+use tokio::{sync::Notify, task::JoinSet};
 
 use super::common_tests::{
     test_contracts_page_ok, test_counters_ok, test_lines_ok, test_main_page_ok,
@@ -16,7 +16,7 @@ use super::common_tests::{
 };
 use crate::{
     common::{get_test_stats_settings, run_consolidated_tests},
-    it::mock_blockscout_simple::{get_mock_blockscout, STATS_INIT_WAIT_S},
+    it::mock_blockscout_simple::get_mock_blockscout,
 };
 
 #[tokio::test]
@@ -29,10 +29,10 @@ pub async fn run_fully_initialized_stats_tests() {
     std::env::set_var("STATS__CONFIG", "./tests/config/test.toml");
     let (settings, base) = get_test_stats_settings(&stats_db, blockscout_db, &blockscout_api);
 
-    init_server(|| stats(settings), &base).await;
-
-    // Sleep until server will start and calculate all values
-    sleep(Duration::from_secs(STATS_INIT_WAIT_S)).await;
+    let stats_init_finished = Arc::new(Notify::new());
+    let notify_handle = stats_init_finished.clone();
+    init_server(move || stats(settings, Some(notify_handle)), &base).await;
+    stats_init_finished.notified().await;
 
     let tests: JoinSet<_> = [
         test_lines_ok(base.clone(), true, true).boxed(),
