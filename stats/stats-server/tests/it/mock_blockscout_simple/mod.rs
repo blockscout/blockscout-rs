96a5aa441c130b6a3edb3d42c806a9e563cc8f8b
Kirill Ivanov <bragov4ik@hotmail.com>
2025-02-19 12:44:38 +0300

    bring back the old tests

--- stats/stats-server/tests/it/mock_blockscout_simple/mod.rs
+++ stats/stats-server/tests/it/mock_blockscout_simple/mod.rs
@@ -1,13 +1,13 @@
-//! Tests for fully initialized blockscout without reindexing
+//! Tests for fully initialized blockscout without reindexing.
+//!
+//! The tests also must not change the state of blockscout db.
 
 use std::str::FromStr;
 
+use blockscout_service_launcher::test_database::TestDbGuard;
 use chrono::NaiveDate;
-use futures::FutureExt;
 use stats::tests::{init_db::init_db_blockscout, mock_blockscout::fill_mock_blockscout_data};
-use tokio::task::JoinSet;
-
-use crate::common::run_consolidated_tests;
+use tokio::sync::OnceCell;
 
 mod common_tests;
 mod stats_full;
@@ -15,27 +15,18 @@ mod stats_no_arbitrum;
 mod stats_not_indexed;
 mod stats_not_updated;
 
-/// Tests that do not change the state of blockscout db
-#[tokio::test]
-#[ignore = "needs database"]
-async fn tests_with_mock_blockscout() {
-    let test_name = "tests_with_mock_blockscout";
-    let blockscout_db = init_db_blockscout(test_name).await;
-    fill_mock_blockscout_data(&blockscout_db, NaiveDate::from_str("2023-03-01").unwrap()).await;
+const STATS_INIT_WAIT_S: u64 = 8;
+static MOCK_BLOCKSCOUT: OnceCell<TestDbGuard> = OnceCell::const_new();
 
-    let tests: JoinSet<_> = [
-        stats_full::run_fully_initialized_stats_tests(blockscout_db.clone(), "0").boxed(),
-        stats_full::run_fully_initialized_stats_tests(blockscout_db.clone(), "1").boxed(),
-        stats_full::run_fully_initialized_stats_tests(blockscout_db.clone(), "2").boxed(),
-        stats_full::run_fully_initialized_stats_tests(blockscout_db.clone(), "3").boxed(),
-        stats_full::run_fully_initialized_stats_tests(blockscout_db.clone(), "4").boxed(),
-        // stats_no_arbitrum::run_chart_pages_tests_with_disabled_arbitrum(blockscout_db.clone())
-        //     .boxed(),
-        // stats_not_indexed::run_tests_with_charts_uninitialized(blockscout_db.clone()).boxed(),
-        // stats_not_indexed::run_tests_with_user_ops_not_indexed(blockscout_db.clone()).boxed(),
-        // stats_not_updated::run_tests_with_charts_not_updated(blockscout_db).boxed(),
-    ]
-    .into_iter()
-    .collect();
-    run_consolidated_tests(tests, test_name).await;
+/// All tests using this must not change the state of blockscout db.
+async fn get_mock_blockscout() -> &'static TestDbGuard {
+    MOCK_BLOCKSCOUT
+        .get_or_init(|| async {
+            let test_name = "tests_with_mock_blockscout";
+            let blockscout_db = init_db_blockscout(test_name).await;
+            fill_mock_blockscout_data(&blockscout_db, NaiveDate::from_str("2023-03-01").unwrap())
+                .await;
+            blockscout_db
+        })
+        .await
 }
