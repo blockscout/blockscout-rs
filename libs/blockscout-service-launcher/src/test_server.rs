45f098c06a9cf41cfed076988133330f99dbc7cf
Kirill Ivanov <bragov4ik@hotmail.com>
2025-02-25 13:24:16 +0300

    don't print error's url just in case

--- libs/blockscout-service-launcher/src/test_server.rs
+++ libs/blockscout-service-launcher/src/test_server.rs
@@ -109,7 +109,7 @@ async fn send_annotated_request<Response: for<'a> serde::Deserialize<'a>>(
     let response = request
         .send()
         .await
-        .unwrap_or_else(|e| panic!("{annotation}Failed to send request: {e}"));
+        .unwrap_or_else(|e| panic!("{annotation}Failed to send request: {}", e.without_url()));
 
     // Assert that status code is success
     if !response.status().is_success() {
