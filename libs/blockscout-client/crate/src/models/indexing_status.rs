6b5e758ba67b1420e25fd1c5a3776de856645777
Rim Rakhimov <rimrakhimov@gmail.com>
2025-02-27 10:02:35 +0400

    fix(blockscout-client): make `to` field in `Transaction` model optional (#1243)
    * fix(blockscout-client): make `to` field in `Transaction` model optional
    
    * fix(transactions): rename `block` to `block_number`
    
    * feat(blockscout-client): update transactions model
    
    * feat(blockscout-client): update apis content handling
    
    * feat(blockscout-client): remove tx aliases from models
    
    * chore(blockscout-client): add cargo fmt into 'generate-from-remote' command
    
    * feat(blockscout-client): regenerate transactions model
    
    * feat(blockscout-client): exclude smart-contracts model from ignored
    
    * feat(blockscout-client): update address_params model
    
    * feat(blockscout-client): update block model
    
    * feat(blockscout-client): exclude decoded_input_parameter model from ignored
    
    * feat(blockscout-client): update internal_transaction model
    
    * feat(blockscout-client): exclude reward model from ignored
    
    * feat(blockscout-client): update smart_contract_for_list model
    
    * revert(blockscout-client): return apis implementation without content-type parsing
    
    * fix(blockscout-client): make SmartContractForList.transaction_count optional
    
    * fix(blockscout-client): update TokenTransfer struct

--- libs/blockscout-client/crate/src/models/indexing_status.rs
+++ libs/blockscout-client/crate/src/models/indexing_status.rs
@@ -19,8 +19,11 @@ pub struct IndexingStatus {
     pub finished_indexing_blocks: bool,
     #[serde(rename = "indexed_blocks_ratio")]
     pub indexed_blocks_ratio: Option<String>, // changed
-    #[serde(rename = "indexed_internal_transactions_ratio")]
-    pub indexed_internal_transactions_ratio: Option<String>, // changed
+    #[serde(
+        rename = "indexed_internal_transactions_ratio",
+        deserialize_with = "Option::deserialize"
+    )]
+    pub indexed_internal_transactions_ratio: Option<String>,
 }
 
 impl IndexingStatus {
