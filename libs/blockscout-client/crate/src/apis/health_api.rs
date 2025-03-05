4dc7593865ca18e59d4436cb58cc4921edf94ddc
sevenzing <lymarenko.lev@gmail.com>
2025-03-05 13:57:12 +0300

    add v1 health

--- libs/blockscout-client/crate/src/apis/health_api.rs
+++ libs/blockscout-client/crate/src/apis/health_api.rs
@@ -17,7 +17,13 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(untagged)]
 pub enum HealthError {
-    DefaultResponse(models::RpcStatus),
+    UnknownValue(serde_json::Value),
+}
+
+/// struct for typed errors of method [`health_v1`]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum HealthV1Error {
     UnknownValue(serde_json::Value),
 }
 
@@ -42,6 +48,35 @@ pub async fn health(
     } else {
         let content = resp.text().await?;
         let entity: Option<HealthError> = serde_json::from_str(&content).ok();
+        Err(Error::ResponseError(ResponseContent {
+            status,
+            content,
+            entity,
+        }))
+    }
+}
+
+pub async fn health_v1(
+    configuration: &configuration::Configuration,
+) -> Result<models::V1HealthCheckResponse, Error<HealthV1Error>> {
+    let uri_str = format!("{}/api/v1/health", configuration.base_path);
+    let mut req_builder = configuration.client.request(reqwest::Method::GET, &uri_str);
+
+    if let Some(ref user_agent) = configuration.user_agent {
+        req_builder = req_builder.header(reqwest::header::USER_AGENT, user_agent.clone());
+    }
+
+    let req = req_builder.build()?;
+    let resp = configuration.client.execute(req).await?;
+
+    let status = resp.status();
+
+    if !status.is_client_error() && !status.is_server_error() {
+        let content = resp.text().await?;
+        serde_json::from_str(&content).map_err(Error::from)
+    } else {
+        let content = resp.text().await?;
+        let entity: Option<HealthV1Error> = serde_json::from_str(&content).ok();
         Err(Error::ResponseError(ResponseContent {
             status,
             content,
