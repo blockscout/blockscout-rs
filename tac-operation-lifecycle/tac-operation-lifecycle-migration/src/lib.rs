087556d3667c09b0b83fd9ef4962100c75c1627d
EvgenKor <evgen2k7@yandex.ru>
2025-05-14 08:03:09 +0300

    Add operation_meta_info table in migration

--- tac-operation-lifecycle/tac-operation-lifecycle-migration/src/lib.rs
+++ tac-operation-lifecycle/tac-operation-lifecycle-migration/src/lib.rs
@@ -1,7 +1,7 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20220101_000001_create_table;
-mod m20250512_135947_add_operation_sender;
+mod m20250512_135947_add_operation_metainfo;
 
 pub struct Migrator;
 
@@ -10,7 +10,7 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20220101_000001_create_table::Migration),
-            Box::new(m20250512_135947_add_operation_sender::Migration),
+            Box::new(m20250512_135947_add_operation_metainfo::Migration),
         ]
     }
 }
