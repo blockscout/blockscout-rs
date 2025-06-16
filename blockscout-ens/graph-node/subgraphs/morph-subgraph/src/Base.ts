08fc09f2c41358ce7b2bdd42ab54cacc6ec9f40d
zbr010521 <118599320+zbr010521@users.noreply.github.com>
2025-06-17 02:56:32 +0800

    bug fix

--- blockscout-ens/graph-node/subgraphs/morph-subgraph/src/Base.ts
+++ blockscout-ens/graph-node/subgraphs/morph-subgraph/src/Base.ts
@@ -68,8 +68,12 @@ export function handleNameRegistered(event: NameRegisteredEvent): void {
 
 export function handleNameRenewed(event: NameRenewedEvent): void {
   let label = uint256ToByteArray(event.params.id);
-  let registration = Registration.load(label.toHex())!;
-  let domain = Domain.load(crypto.keccak256(concat(rootNode, label)).toHex())!;
+  let registration = Registration.load(label.toHex());
+  let domain = Domain.load(crypto.keccak256(concat(rootNode, label)).toHex());
+
+  if (registration === null || domain === null) {
+    return;
+  }
 
   registration.expiryDate = event.params.expires;
   domain.expiryDate = event.params.expires.plus(GRACE_PERIOD_SECONDS);
