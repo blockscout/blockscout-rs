de8afba03dfa74a808df21b34bd8cb4005139e76
Ilyak <ilyaprgrmist@gmail.com>
2025-07-02 13:05:30 +0200

    fix: updated imports from user crate

--- libs/blockscout-auth/src/lib.rs
+++ libs/blockscout-auth/src/lib.rs
@@ -5,5 +5,5 @@ mod user;
 
 pub use auth::{auth_from_metadata, auth_from_tokens, AuthSuccess, Error};
 pub use mock::{init_mocked_blockscout_auth_service, MockUser};
-pub use common::{extract_csrf_token, extract_jwt, AuthError, CommonError, build_http_headers};
-use user::{get_user_info_from_metadata, UserInfo};
+pub use common::{extract_csrf_token, extract_jwt, AuthError as CommonError, build_http_headers};
+pub use user::{get_user_info_from_metadata, UserInfo};
