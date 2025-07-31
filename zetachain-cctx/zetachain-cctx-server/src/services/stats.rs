684ae5cf1db86ebcdc61b21101e04da4783647d8
vladimir <vladimir@blockscout.com>
2025-07-31 20:08:26 +0300

    upd stats

--- zetachain-cctx/zetachain-cctx-server/src/services/stats.rs
+++ zetachain-cctx/zetachain-cctx-server/src/services/stats.rs
@@ -25,7 +25,7 @@ impl Stats for StatsService {
     async fn get_sync_progress(&self, _request: Request<GetSyncProgressRequest>) -> Result<Response<GetSyncProgressResponse>, Status> {
         let sync_progress = self.db.get_sync_progress().await.map_err(|e| Status::internal(e.to_string()))?;
         Ok(Response::new(GetSyncProgressResponse {
-            historic_watermark_timestamp: sync_progress.historical_watermark_timestamp.and_utc().timestamp(),
+            historic_watermark_timestamp: sync_progress.historical_watermark_timestamp.and_utc().to_string(),
             pending_status_updates_count: sync_progress.pending_status_updates_count,
             pending_cctxs_count: sync_progress.pending_cctxs_count,
             realtime_gaps_count: sync_progress.realtime_gaps_count,
