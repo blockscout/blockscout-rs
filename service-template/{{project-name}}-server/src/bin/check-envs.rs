a103e8be28798cd953cbf912c55522dbfc7408e5
sevenzing <lymarenko.lev@gmail.com>
2025-08-01 15:57:08 +0300

    fix PR comments

--- service-template/{{project-name}}-server/src/bin/check-envs.rs
+++ service-template/{{project-name}}-server/src/bin/check-envs.rs
@@ -4,12 +4,12 @@ use env_collector::{run_env_collector_cli, EnvCollectorSettingsBuilder, PrefixFi
 fn main() {
     run_env_collector_cli::<Settings>(
         EnvCollectorSettingsBuilder::default()
-            .service_name("{{crate_name | upcase}}")
+            .service_name("{{CRATE_NAME}}")
             .markdown_path("README.md")
             .config_path("{{project-name}}-server/config/example.toml")
             .vars_filter(PrefixFilter::blacklist(&[
-                "{{crate_name | upcase}}__SERVER",
-                "{{crate_name | upcase}}__JAEGER",
+                "{{CRATE_NAME}}__SERVER",
+                "{{CRATE_NAME}}__JAEGER",
             ]))
             .build()
             .expect("failed to build env collector settings"),
