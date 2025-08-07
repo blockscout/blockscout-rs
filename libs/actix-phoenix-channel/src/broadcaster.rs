94a6dbd808fec85acef523f039ad70d85b5bf791
Leonid Tyurin <lenyatyurin162586@gmail.com>
2025-08-07 18:19:57 +0700

    fix: fmt

--- libs/actix-phoenix-channel/src/broadcaster.rs
+++ libs/actix-phoenix-channel/src/broadcaster.rs
@@ -51,11 +51,11 @@ where
     C: Clone,
 {
     fn activate(&mut self) {
-        if let Receiver::Inactive(_) = self {
-            if let Receiver::Inactive(inactive) = mem::replace(self, Self::Activating) {
-                *self = Receiver::Active(inactive.activate());
-            };
-        }
+        if let Receiver::Inactive(_) = self
+            && let Receiver::Inactive(inactive) = mem::replace(self, Self::Activating)
+        {
+            *self = Receiver::Active(inactive.activate());
+        };
     }
 }
 
