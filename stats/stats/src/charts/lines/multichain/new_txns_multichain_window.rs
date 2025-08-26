4a4c2ae61d0245c4768665032a9a8b2aa128bfe1
EvgenKor <evgen2k7@yandex.ru>
2025-08-26 12:19:05 +0300

    Tune test

--- stats/stats/src/charts/lines/multichain/new_txns_multichain_window.rs
+++ stats/stats/src/charts/lines/multichain/new_txns_multichain_window.rs
@@ -193,9 +193,8 @@ mod tests {
             ]),
         );
 
-        add_mock_multichain_data(&indexer, NaiveDate::from_str("2023-02-15").unwrap()).await;
-
-        let current_time = dt("2023-03-05T00:00:00").and_utc();
+        add_mock_multichain_data(&indexer, NaiveDate::from_str("2023-03-05").unwrap()).await;
+        let current_time = dt("2023-03-06T00:00:00").and_utc();
         parameters.update_time_override = Some(current_time);
         let cx = UpdateContext::from_params_now_or_override(parameters);
         NewTxnsMultichainWindow::update_recursively(&cx)
@@ -215,9 +214,8 @@ mod tests {
             &map_str_tuple_to_owned(vec![
                 // values outside the window are removed
                 // new values within the window are added
-                ("2023-02-03", "49"),
                 ("2023-02-04", "60"),
-                ("2023-02-15", "10"),
+                ("2023-03-05", "10"),
             ]),
         );
     }
