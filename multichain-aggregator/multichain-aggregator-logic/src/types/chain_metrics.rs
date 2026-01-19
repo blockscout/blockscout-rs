5df354786cb41e8ebba55be7e9f15f662d346c76
Leonid Tyurin <lenyatyurin162586@gmail.com>
2026-01-19 11:17:07 +0100

    feat(multi): add cluster stats (#1598)
    * feat: add cluster stats
    
    * refactor: review comments

---
+++ multichain-aggregator/multichain-aggregator-logic/src/types/chain_metrics.rs
@@ -0,0 +1,42 @@
+use super::ChainId;
+use crate::proto;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+pub struct WeeklyMetric {
+    pub current_full_week: i64,
+    pub previous_full_week: i64,
+    /// Week-over-week difference as a percentage (e.g., "42.2" for 42.2% increase).
+    pub wow_diff_percent: f64,
+}
+
+impl From<WeeklyMetric> for proto::WeeklyMetric {
+    fn from(v: WeeklyMetric) -> Self {
+        Self {
+            current_full_week: v.current_full_week.to_string(),
+            previous_full_week: v.previous_full_week.to_string(),
+            wow_diff_percent: format!("{:.2}", v.wow_diff_percent),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ChainMetrics {
+    pub chain_id: ChainId,
+    pub tps: Option<f64>,
+    pub new_addresses: Option<WeeklyMetric>,
+    pub daily_transactions: Option<WeeklyMetric>,
+    pub active_accounts: Option<WeeklyMetric>,
+}
+
+impl From<ChainMetrics> for proto::ChainMetrics {
+    fn from(v: ChainMetrics) -> Self {
+        Self {
+            chain_id: v.chain_id.to_string(),
+            tps: v.tps.map(|tps| format!("{:.2}", tps)),
+            new_addresses: v.new_addresses.map(|m| m.into()),
+            daily_transactions: v.daily_transactions.map(|m| m.into()),
+            active_accounts: v.active_accounts.map(|m| m.into()),
+        }
+    }
+}
