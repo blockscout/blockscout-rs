5200c109aec8fe9f6311fe4ad27614b3ea33dcad
Rob Domains <robdncom@gmail.com>
2026-09-26 15:33:41 +0300

    Update RegistrarController.ts

--- blockscout-ens/graph-node/subgraphs/doti-subgraph/src/RegistrarController.ts
+++ blockscout-ens/graph-node/subgraphs/doti-subgraph/src/RegistrarController.ts
@@ -28,11 +28,42 @@ var rootNode: ByteArray = byteArrayFromHex(BASE_NODE_HASH);
 export function handleNameRegisteredByController(
   event: ControllerNameRegisteredEvent
 ): void {
-  setNamePreimage(
-    event.params.name,
-    event.params.label,
-    event.params.baseCost
-  );
+  let account = new Account(event.params.owner.toHex());
+  account.save();
+
+  let domainId = crypto.keccak256(concat(rootNode, event.params.label)).toHex();
+  let domain = Domain.load(domainId);
+  if (domain == null) {
+    domain = new Domain(domainId);
+    domain.createdAt = event.block.timestamp;
+    domain.subdomainCount = 0;
+    domain.storedOffchain = false;
+    domain.resolvedWithWildcard = false;
+    domain.owner = account.id;
+    domain.registrant = account.id;
+    domain.isMigrated = true;
+  }
+
+  if (checkValidLabel(event.params.name)) {
+    domain.labelName = event.params.name;
+    domain.labelhash = event.params.label;
+    domain.name = event.params.name + BASE_NODE;
+  }
+  domain.save();
+
+  let registration = Registration.load(event.params.label.toHex());
+  if (registration == null) {
+    registration = new Registration(event.params.label.toHex());
+    registration.domain = domain.id;
+    registration.registrationDate = event.block.timestamp;
+    registration.registrant = account.id;
+  }
+  if (checkValidLabel(event.params.name)) {
+    registration.labelName = event.params.name;
+  }
+  registration.cost = event.params.baseCost;
+  registration.expiryDate = event.params.expires;
+  registration.save();
 }
 
 export function handleNameRenewedByController(
