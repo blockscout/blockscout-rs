ef73572295388cf50d99a30f8463069a612bc88b
Rob Domains <robdncom@gmail.com>
2026-09-26 15:46:20 +0300

    Add files via upload

--- blockscout-ens/graph-node/subgraphs/doti-subgraph/tests/utils.test.ts
+++ blockscout-ens/graph-node/subgraphs/doti-subgraph/tests/utils.test.ts
@@ -7,10 +7,15 @@ import { hashByName } from "../src/utils";
 import { Bytes } from "@graphprotocol/graph-ts";
 
 describe("Utils", () => {
-  test("Name hashing works", () => {
+  test("Name hashing works for root tld", () => {
     assert.bytesEquals(
       Bytes.fromHexString('0x96b16e885d568c078028f8feef27a718c0e2a3cf42145b2100806cb1f07f4bb7'),
       Bytes.fromByteArray(hashByName('i'))
     );
+  });
+
+  test("Name hashing works for domain name", () => {
+    let hashed = hashByName('test.i');
+    assert.assertTrue(hashed.length == 32);
   });
 });
